b68bdab32dd9f7895b0052741d470597a59a256a
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-08-12 17:14:17 +0900

    initial commit

---
+++ backend/src/main/java/com/woowacourse/edd/EddApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.edd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EddApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/woowacourse/edd/EddApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woowacourse.edd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EddApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
