bdc7ce7a3f815aa5da2392393c041e7fafac204f
kmdngyu <kmdngyu@gmail.com>
2019-08-12 15:58:53 +0900

    initial commit

---
+++ src/test/java/techcourse/w3/woostagram/WoostagramApplicationTests.java
@@ -0,0 +1,16 @@
+package techcourse.w3.woostagram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WoostagramApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
