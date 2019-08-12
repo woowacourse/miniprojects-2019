95598422e8e93618ad12b63d1e3d3b6d4ba1cb97
Seongmo Jeon <oeeen3@gmail.com>
2019-08-13 08:51:21 +0900

    feat(base): init project

---
+++ src/main/java/com/woowacourse/sunbook/SunbookApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.sunbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woowacourse/sunbook/SunbookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woowacourse.sunbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SunbookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
