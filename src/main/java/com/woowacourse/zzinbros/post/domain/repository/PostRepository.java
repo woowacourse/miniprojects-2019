e14f31b2a28e3ee71a3545c2a5c571319053c917
gch01410 <gch01410@naver.com>
2019-08-13 15:59:41 +0900

    feat(domain): PostRepository 생성
    1. JpaRepository

---
+++ src/main/java/com/woowacourse/zzinbros/post/domain/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.woowacourse.zzinbros.post.domain.repository;
+
+import com.woowacourse.zzinbros.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
