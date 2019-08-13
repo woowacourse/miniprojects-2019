19d65b0f4cd7993cc17ee3f56e783d46690161db
andole87 <andole87@gmail.com>
2019-08-13 13:50:38 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/woowacourse/zzinbros/ZzinbrosApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.zzinbros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzinbrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzinbrosApplication.class, args);
+	}
+
+}
