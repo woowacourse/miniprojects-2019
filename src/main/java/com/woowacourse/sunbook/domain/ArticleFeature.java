5e7a4241bddebbb4a56eb4e796ed16e0fe44cb5e
Seongmo Jeon <oeeen3@gmail.com>
2019-08-13 21:39:50 +0900

    feat: 이미지 및 영상 url 유효성 검사

--- src/main/java/com/woowacourse/sunbook/domain/ArticleFeature.java
+++ src/main/java/com/woowacourse/sunbook/domain/ArticleFeature.java
@@ -2,14 +2,24 @@ package com.woowacourse.sunbook.domain;
 
 import lombok.Getter;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Getter
 @Embeddable
 public class ArticleFeature {
 
+    private final static Pattern URL_PATTERN = Pattern.compile("^(https?):\\/\\/([^:\\/\\s]+)(:([^\\/]*))?((\\/[^\\s/\\/]+)*)?\\/?([^#\\s\\?]*)(\\?([^#\\s]*))?(#(\\w*))?$");
+
+    @Column(nullable = false)
     private final String contents;
+
+    @Column(nullable = false)
     private final String imageUrl;
+
+    @Column(nullable = false)
     private final String videoUrl;
 
     private ArticleFeature() {
@@ -28,6 +38,13 @@ public class ArticleFeature {
     private void empty(String contents, String imageUrl, String videoUrl) {
         if ("".equals(contents) && "".equals(imageUrl) && "".equals(videoUrl)) {
             throw new IllegalArgumentException("내용이 없습니다.");
+        }
+    }
+
+    private void validateUrl(String url) {
+        Matcher matcher = URL_PATTERN.matcher(url);
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("유효하지 않은 url입니다.");
         }
     }
 }
