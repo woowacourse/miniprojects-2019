6b50029ec4cbc4cd579427622c8203d456ab7103
skygl <jupiter9501@gmal.com>
2019-08-13 14:36:04 +0900

    feat: User 도메인 생성

--- src/main/java/com/wootecobook/turkey/user/domain/User.java
+++ src/main/java/com/wootecobook/turkey/user/domain/User.java
@@ -1,7 +1,39 @@
 package com.wootecobook.turkey.user.domain;
 
 import com.wootecobook.turkey.commons.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class User extends BaseEntity {
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^([A-Za-z가-힣]{2,10})$")
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "^([a-zA-Z0-9!@#$%^&*]{8,})$")
+    private String password;
+
+    @Builder
+    public User(String email, String name, String password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
 
 }
