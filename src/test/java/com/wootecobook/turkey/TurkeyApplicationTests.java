9ebc584d4769a6c146f1b6f10a65c30740b0291b
pkch93 <star_final@naver.com>
2019-08-13 12:11:22 +0900

    refactor: junit4 어노테이션 junit5로 변경

--- src/test/java/com/wootecobook/turkey/TurkeyApplicationTests.java
+++ src/test/java/com/wootecobook/turkey/TurkeyApplicationTests.java
@@ -1,16 +1,14 @@
 package com.wootecobook.turkey;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class TurkeyApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
