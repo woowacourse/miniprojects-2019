afcf3a85fc9f0cb20d02e958c244354fb9ea5bf4
ep1stas1s <masloz@konkuk.ac.kr>
2019-08-14 03:30:00 +0900

    feat: 제약사항 추가

--- src/main/java/com/woowacourse/dsgram/domain/UserBasicInfo.java
+++ src/main/java/com/woowacourse/dsgram/domain/UserBasicInfo.java
@@ -1,23 +1,40 @@
 package com.woowacourse.dsgram.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Embeddable
 @Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserBasicInfo {
+    @Email
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Size(min = 2, max = 10)
     @Column(nullable = false, unique = true)
     private String nickName;
 
+    @Size(min = 2, max = 10)
     @Column(nullable = false)
     private String userName;
 
+    @Size(min = 4, max = 16)
     @Column(nullable = false)
     private String password;
 
+    public UserBasicInfo(String email, String nickName, String userName, String password) {
+        this.email = email;
+        this.nickName = nickName;
+        this.userName = userName;
+        this.password = password;
+    }
 }
