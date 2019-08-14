8f923a8519fd1687a6a8f743818ba1f78d73cc42
os94 <ss1511@naver.com>
2019-08-14 21:24:25 +0900

    refactor: 게시글 수정 페이지 이동 기능 보완

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,8 @@
 const writeArticleButton = document.getElementById("write-article-button");
-writeArticleButton.addEventListener("click", writeArticle());
 
-const writeArticle = function() {
+function writeArticle() {
     // todo : 로그인 확인 기능 추가
-
     location.href = "/articles/writing";
 };
+
+writeArticleButton.addEventListener("click", writeArticle);
