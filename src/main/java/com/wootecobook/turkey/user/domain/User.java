51a26723462a92f2a2ee571cd2f7553fd8b16fd4
skygl <jupiter9501@gmal.com>
2019-08-14 15:58:34 +0900

    refactor: User 검증 방식 수정

--- src/main/java/com/wootecobook/turkey/user/domain/User.java
+++ src/main/java/com/wootecobook/turkey/user/domain/User.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Entity
 @Getter
@@ -17,22 +15,21 @@ import javax.validation.constraints.Pattern;
 public class User extends BaseEntity {
 
     @Email
-    @NotBlank
     @Column(unique = true)
     private String email;
 
-    @NotBlank
     @Column
-    @Pattern(regexp = "^([A-Za-z가-힣]{2,10})$")
     private String name;
 
-    @NotBlank
     @Column
-    @Pattern(regexp = "^([a-zA-Z0-9!@#$%^&*]{8,})$")
     private String password;
 
     @Builder
     public User(String email, String name, String password) {
+        UserValidator.validateEmail(email);
+        UserValidator.validateName(name);
+        UserValidator.validatePassword(password);
+
         this.email = email;
         this.name = name;
         this.password = password;
