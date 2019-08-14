afa94f5184e56d304ddf7b7ce8aa9c1009d5c22b
school0bhy <house1217@ajou.ac.kr>
2019-08-14 11:08:39 +0900

    refactor: Mock 테스트를 DataJpaTest로 변경

--- src/test/java/com/wootecobook/turkey/post/service/PostServiceTest.java
+++ src/test/java/com/wootecobook/turkey/post/service/PostServiceTest.java
@@ -1,72 +1,80 @@
 package com.wootecobook.turkey.post.service;
 
-import com.wootecobook.turkey.commons.resolver.UserSession;
 import com.wootecobook.turkey.post.domain.Contents;
 import com.wootecobook.turkey.post.domain.Post;
 import com.wootecobook.turkey.post.domain.PostRepository;
 import com.wootecobook.turkey.post.service.dto.PostRequest;
 import com.wootecobook.turkey.post.service.dto.PostResponse;
 import com.wootecobook.turkey.post.service.exception.NotExistPostException;
-import com.wootecobook.turkey.user.domain.User;
-import com.wootecobook.turkey.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@DataJpaTest
 class PostServiceTest {
 
-    @Mock
+    @Autowired
     private PostRepository postRepository;
 
-    @Mock
-    private UserService userService;
-
-    @InjectMocks
     private PostService postService;
 
-    String testPostContentText;
-    Contents testContents;
-    Post post;
+    private String testPostContentText;
+    private Contents testContents;
+    private Post post;
 
     @BeforeEach
     void setUp() {
+        postService = new PostService(postRepository);
+
         testPostContentText = "hello";
         testContents = new Contents(testPostContentText);
-        post = new Post(testContents, new User());
+        post = new Post(testContents);
     }
 
     @Test
     void post_생성_테스트() {
-        when(postRepository.save(post)).thenReturn(post);
-
-        PostResponse result = postService.save(new PostRequest(testPostContentText), new UserSession());
+        PostResponse result = postService.save(new PostRequest(testPostContentText));
 
         assertThat(result.getContents()).isEqualTo(testContents);
     }
 
     @Test
     void post_조회_테스트() {
-        long testId = 1L;
-        when(postRepository.findById(testId)).thenReturn(Optional.of(post));
+        PostResponse savedPost = postService.save(new PostRequest(testPostContentText));
+        long testId = savedPost.getId();
 
         Post testPost = postService.findById(testId);
 
         assertThat(testPost.getContents()).isEqualTo(testContents);
-        assertThrows(NotExistPostException.class, () -> postService.findById(2L));
+        assertThrows(NotExistPostException.class, () -> postService.findById(testId + 1));
     }
 
     @Test
     void post_페이징_테스트() {
+        int pageNum = 0;
+        IntStream.rangeClosed(1, 100).forEach(i ->
+                postService.save(new PostRequest("hello" + i)));
 
+        Page<PostResponse> pageResponse = postService.findPostResponses(PageRequest.of(pageNum, 10));
+
+        assertThat(pageResponse.getTotalElements()).isEqualTo(100);
+        assertThat(pageResponse.getTotalPages()).isEqualTo(10);
+        assertThat(pageResponse.getNumber()).isEqualTo(pageNum);
+
+        List<PostResponse> postResponses = pageResponse.getContent();
+
+        for (int i = 1; i <= 10; i++) {
+            PostResponse postResponse = postResponses.get(i - 1);
+            assertThat(postResponse.getContents()).isEqualTo(new Contents("hello" + i));
+        }
     }
 }
