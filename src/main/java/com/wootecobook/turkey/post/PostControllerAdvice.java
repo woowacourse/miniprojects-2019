0574659f27673029f00661b23a4338ab69c62dd7
school0bhy <house1217@ajou.ac.kr>
2019-08-15 13:49:52 +0900

    refactor: PostControllerAdvice 피드백 반영 - RestControllerAdvice - ResponseEntity 팩토리 메서드 사용

--- src/main/java/com/wootecobook/turkey/post/PostControllerAdvice.java
+++ src/main/java/com/wootecobook/turkey/post/PostControllerAdvice.java
@@ -3,22 +3,19 @@ package com.wootecobook.turkey.post;
 import com.wootecobook.turkey.commons.ErrorMessage;
 import com.wootecobook.turkey.post.controller.PostApiController;
 import com.wootecobook.turkey.post.controller.exception.PostBadRequestException;
-import com.wootecobook.turkey.post.service.exception.NotExistPostException;
-import org.springframework.http.HttpStatus;
+import com.wootecobook.turkey.post.service.exception.NotFoundPostException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice(basePackageClasses = {PostApiController.class})
+@RestControllerAdvice(basePackageClasses = {PostApiController.class})
 public class PostControllerAdvice {
 
-    @ExceptionHandler({PostBadRequestException.class, NotExistPostException.class})
-    @ResponseBody
+    @ExceptionHandler({PostBadRequestException.class, NotFoundPostException.class})
     public ResponseEntity<ErrorMessage> handleException(RuntimeException exception) {
         ErrorMessage errorMessage = new ErrorMessage(exception.getMessage());
 
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 
 }
