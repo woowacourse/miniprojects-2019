3ef3cca4f20f3f2302c250ce1c9f8dd2f33bfa81
school0bhy <house1217@ajou.ac.kr>
2019-08-15 10:23:40 +0900

    refactor: PostService에 @Transactional 적용

--- src/main/java/com/wootecobook/turkey/post/service/PostService.java
+++ src/main/java/com/wootecobook/turkey/post/service/PostService.java
@@ -8,8 +8,10 @@ import com.wootecobook.turkey.post.service.exception.NotExistPostException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class PostService {
 
     private final PostRepository postRepository;
