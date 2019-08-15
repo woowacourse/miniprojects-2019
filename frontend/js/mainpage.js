e350df0e5a6f2a484be80b7c6f9d4b73431b68c8
Shin Jinwoo <bluesky0255@gmail.com>
2019-08-15 15:25:28 +0900

    Revert "[refactor] delete window.onloadmethod, localhost url (#18)" (#19)
    This reverts commit d2ea40d1c4fa5b9681115c30f5dc92f75ede0518.

--- frontend/js/mainpage.js
+++ frontend/js/mainpage.js
@@ -1,7 +1,10 @@
 loadDateVideoCards = function () {
-    api.get("/api/v1/videos?filter=view&page=1&limit=6")
+        api.get("http://localhost:8080/api/v1/videos?filter=view&page=1&limit=6")
         .then(response => response.json())
-        .then(json => addVideoCardTemplates(json, "dateVideoCard"))
+        .then(json => addVideoCardTemplates(json,"dateVideoCard") )
+    }
+    
+window.onload = function () {
+     loadDateVideoCards()
 }
-loadDateVideoCards()
 
