adc394099250f3282576f8959ce6988b8097bb4b
kangmin46 <40996389+kangmin46@users.noreply.github.com>
2019-08-15 15:33:33 +0900

    [refactor] delete localhost url, window onload (#20)

--- frontend/js/mainpage.js
+++ frontend/js/mainpage.js
@@ -1,10 +1,7 @@
 loadDateVideoCards = function () {
-        api.get("http://localhost:8080/api/v1/videos?filter=view&page=1&limit=6")
+        api.get("/api/v1/videos?filter=view&page=1&limit=6")
         .then(response => response.json())
         .then(json => addVideoCardTemplates(json,"dateVideoCard") )
     }
-    
-window.onload = function () {
-     loadDateVideoCards()
-}
+    loadDateVideoCards()
 
