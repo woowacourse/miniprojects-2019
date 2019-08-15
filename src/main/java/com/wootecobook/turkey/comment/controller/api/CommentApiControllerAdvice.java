b7c9d013347c917af7687d0729c40ae72ebd602b
DaejunBae <dpudpu11@gmail.com>
2019-08-15 22:16:35 +0900

    refactor: CommentApiControllerAdvice  @ExceptionHandler 변경 변경 전 - @ExceptionHandler({RuntimeException.class}) 변경 후 - @ExceptionHandler({ContextNotEmptyException.class, CommentSaveException.class, CommentAuthException.class})

--- src/main/java/com/wootecobook/turkey/comment/controller/api/CommentApiControllerAdvice.java
+++ src/main/java/com/wootecobook/turkey/comment/controller/api/CommentApiControllerAdvice.java
@@ -1,16 +1,19 @@
 package com.wootecobook.turkey.comment.controller.api;
 
-import com.wootecobook.turkey.user.controller.api.ErrorMessage;
+import com.wootecobook.turkey.comment.domain.CommentAuthException;
+import com.wootecobook.turkey.comment.service.exception.CommentSaveException;
+import com.wootecobook.turkey.commons.ErrorMessage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.naming.ContextNotEmptyException;
+
 @RestControllerAdvice(basePackageClasses = {CommentApiController.class})
 public class CommentApiControllerAdvice {
 
-    @ExceptionHandler({RuntimeException.class})
+    @ExceptionHandler({ContextNotEmptyException.class, CommentSaveException.class, CommentAuthException.class})
     private ResponseEntity<ErrorMessage> handleException(Exception e) {
-        //TODO 합치면 commons 패키지로?
         ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
         return ResponseEntity.badRequest().body(errorMessage);
     }
