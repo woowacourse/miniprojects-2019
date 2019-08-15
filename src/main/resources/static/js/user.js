10e33b71583e7c18facc70775bb4aa6333fab67f
ep1stas1s <masloz@konkuk.ac.kr>
2019-08-15 11:14:16 +0900

    refactor: merge javascript file

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -6,11 +6,23 @@ const USER_APP = (() => {
 
         const signUp = () => {
             const signUpButton = document.getElementById('signup');
-            signUpButton.addEventListener('click', userService.saveUser);
+            signUpButton ? signUpButton.addEventListener('click', userService.saveUser) : undefined;
+        };
+
+        const editUser = () => {
+            const editButton = document.getElementById('editButton');
+            editButton ? editButton.addEventListener('click', userService.updateUser) : undefined;
+        };
+
+        const login = () => {
+            const loginButton = document.getElementById('login-button');
+            loginButton ? loginButton.addEventListener('click', userService.login) : undefined;
         };
 
         const init = () => {
             signUp();
+            editUser();
+            login();
         };
 
         return {
@@ -24,6 +36,9 @@ const USER_APP = (() => {
         const nickName = document.getElementById('nickName');
         const userName = document.getElementById('userName');
         const password = document.getElementById('password');
+        const userId = document.getElementById('userId');
+        const webSite = document.getElementById('webSite');
+        const intro = document.getElementById('intro');
 
         const saveUser = function (event) {
             event.preventDefault();
@@ -56,8 +71,71 @@ const USER_APP = (() => {
                 });
         };
 
+        const updateUser = function (event) {
+            event.preventDefault();
+
+            const userDto = {
+                nickName: nickName.value,
+                userName: userName.value,
+                userId: userId.value,
+                webSite: webSite.value,
+                password: password.value,
+                intro: intro.value,
+            };
+
+            fetch('/api/users/' + userId.value, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json; charset=UTF-8'},
+                body: JSON.stringify(userDto)
+            }).then(response => {
+                if (response.status === 200) {
+                    // TODO 슬로스꺼ㄹ로 이동
+                    window.location.href = '/users/' + userId.value + '/edit';
+                }
+
+                if (response.status === 400) {
+                    throw response;
+                }
+            }).catch(error => {
+                error.json()
+                    .then(exception => {
+                        alert(exception.message)
+                    });
+            });
+        };
+
+        const login = function (event) {
+            event.preventDefault();
+
+            let userBasicInfo = {
+                email: email.value,
+                password: password.value,
+            };
+
+            fetch('/api/users/login', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json; charset=UTF-8'},
+                body: JSON.stringify(userBasicInfo)
+            }).then(response => {
+                if (response.status === 200) {
+                    window.location.href = '/';
+                    return;
+                }
+                if (response.status === 400) {
+                    throw response;
+                }
+            }).catch(error => {
+                error.json()
+                    .then(exception => {
+                        alert(exception.message)
+                    });
+            });
+        };
+
         return {
             saveUser: saveUser,
+            updateUser: updateUser,
+            login: login,
         }
     };
 
