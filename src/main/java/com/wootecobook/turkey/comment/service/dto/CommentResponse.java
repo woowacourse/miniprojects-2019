9ef4d1ad46ffd856665e6f0a7bf5c6cbd6f5ba45
DaejunBae <dpudpu11@gmail.com>
2019-08-15 11:24:15 +0900

    refactor: CommentResponse  @JsonDeserialize(using = LocalDateTimeDeserializer.class) 제거

--- src/main/java/com/wootecobook/turkey/comment/service/dto/CommentResponse.java
+++ src/main/java/com/wootecobook/turkey/comment/service/dto/CommentResponse.java
@@ -1,28 +1,19 @@
 package com.wootecobook.turkey.comment.service.dto;
 
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.wootecobook.turkey.comment.domain.Comment;
 import com.wootecobook.turkey.user.service.dto.UserResponse;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 public class CommentResponse {
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
     private Long id;
     private String contents;
     private CommentResponse parent;
     private UserResponse userResponse;
-
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
-
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime updatedAt;
 
     private CommentResponse(final Comment comment) {
