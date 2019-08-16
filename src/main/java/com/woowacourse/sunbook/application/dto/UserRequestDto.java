6d43260963e4fa43b2493685e29d1365e1217d4d
hyun <jhs695@naver.com>
2019-08-16 18:05:45 +0900

    feat: User, Login 테스트 추가

--- src/main/java/com/woowacourse/sunbook/application/dto/UserRequestDto.java
+++ src/main/java/com/woowacourse/sunbook/application/dto/UserRequestDto.java
@@ -3,11 +3,20 @@ package com.woowacourse.sunbook.application.dto;
 import com.woowacourse.sunbook.domain.user.UserEmail;
 import com.woowacourse.sunbook.domain.user.UserName;
 import com.woowacourse.sunbook.domain.user.UserPassword;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserRequestDto {
     private UserEmail userEmail;
     private UserName userName;
     private UserPassword userPassword;
+
+    public UserRequestDto(final UserEmail userEmail, final UserName userName, final UserPassword userPassword) {
+        this.userEmail = userEmail;
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
 }
--- src/test/java/com/woowacourse/sunbook/presentation/UserControllerTest.java
+++ src/test/java/com/woowacourse/sunbook/presentation/UserControllerTest.java
@@ -1,10 +1,34 @@
 package com.woowacourse.sunbook.presentation;
 
+import com.woowacourse.sunbook.presentation.template.TestTemplate;
 import org.junit.jupiter.api.Test;
 
-public class UserControllerTest {
-    @Test
-    public void 유저_생성() {
+public class UserControllerTest extends TestTemplate {
 
+    @Test
+    void 로그인_회원가입_페이지_이동() {
+        webTestClient.get().uri("/")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                ;
+    }
+
+    @Test
+    void 메인_페이지_이동() {
+        webTestClient.get().uri("/sunbook")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                ;
+    }
+
+    @Test
+    void 로그아웃_성공() {
+        webTestClient.post().uri("/signout")
+                .exchange()
+                .expectStatus()
+                .isFound()
+                ;
     }
 }
