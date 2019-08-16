cb7c8f7db119053a4adf27a03e789c35d572a7ff
ep1stas1s <masloz@konkuk.ac.kr>
2019-08-16 19:34:46 +0900

    fix: email 제약조건 변경

--- src/main/java/com/woowacourse/dsgram/service/dto/user/SignUpUserDto.java
+++ src/main/java/com/woowacourse/dsgram/service/dto/user/SignUpUserDto.java
@@ -6,13 +6,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SignUpUserDto {
+    @Pattern(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", message = "이메일 양식")
+    private String email;
+
     @Size(min = 2, max = 10, message = "닉네임은 2~10자")
-    @Email
     private String nickName;
 
     @Size(min = 2, max = 10, message = "이름은 2~10자")
@@ -21,8 +24,6 @@ public class SignUpUserDto {
     @Size(min = 4, max = 16, message = "비밀번호는 4~16자")
     private String password;
 
-    @Email(message = "이메일 양식")
-    private String email;
 
     @Builder
     public SignUpUserDto(@Size(min = 2, max = 10) String nickName, @Size(min = 2, max = 10) String userName, @Size(min = 4, max = 16) String password, @Email String email) {
