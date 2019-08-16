ccb2bef00a1b0951be930cf3f017a77075d0a8b7
hyun <jhs695@naver.com>
2019-08-16 18:07:25 +0900

    chore: ServiceTest ExtendWith 변경

--- src/test/java/com/woowacourse/sunbook/application/ArticleServiceTest.java
+++ src/test/java/com/woowacourse/sunbook/application/ArticleServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
@@ -16,7 +16,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
 public class ArticleServiceTest {
 
     private static final long ARTICLE_ID = 1L;
