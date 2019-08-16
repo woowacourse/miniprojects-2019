6278bd5be85830339a7b024ae0711b84b5096efb
hyun <jhs695@naver.com>
2019-08-16 18:04:29 +0900

    feat: TestTemplate 추가

--- src/test/java/com/woowacourse/sunbook/presentation/ArticleControllerTest.java
+++ src/test/java/com/woowacourse/sunbook/presentation/ArticleControllerTest.java
@@ -1,22 +1,14 @@
 package com.woowacourse.sunbook.presentation;
 
 import com.woowacourse.sunbook.domain.article.ArticleFeature;
+import com.woowacourse.sunbook.presentation.template.TestTemplate;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 import static org.hamcrest.Matchers.hasItem;
 
-@AutoConfigureWebTestClient
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ArticleControllerTest {
+public class ArticleControllerTest extends TestTemplate {
 
     private static final String CONTENTS = "SunBook Contents";
     private static final String IMAGE_URL = "https://file.namu.moe/file/105db7e730e1402c09dcf2b281232df017f0966ba63375176cb0886869b81bf206145de5a7a149a987d6aae2d5230afaae4ca2bf0b418241957942ad4f4a08c8";
@@ -27,8 +19,6 @@ public class ArticleControllerTest {
     private static final String UPDATE_IMAGE_URL = "http://mblogthumb2.phinf.naver.net/MjAxNzA2MDhfODYg/MDAxNDk2ODgyNDE3NDYz.yMs2-E3-GlBu9U_4r2GMnBd1IEgVlWG2Qos9pb-2WWIg.M4JN5W9K2kMt9n76gjYQUKPBGt0eHMXE0UrvWFvr6Vgg.PNG.smartbaedal/18.png?type=w800";
     private static final String UPDATE_VIDEO_URL = "https://youtu.be/4HG_CJzyX6A";
 
-    @Autowired
-    WebTestClient webTestClient;
 
     @Test
     void 게시글_전체_조회() {
---
+++ src/test/java/com/woowacourse/sunbook/presentation/template/TestTemplate.java
@@ -0,0 +1,16 @@
+package com.woowacourse.sunbook.presentation.template;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@AutoConfigureWebTestClient
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestTemplate {
+    @Autowired
+    protected WebTestClient webTestClient;
+}
