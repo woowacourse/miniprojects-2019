8e52f8e973feae48c72087614c42816df477a2e5
DaejunBae <dpudpu11@gmail.com>
2019-08-16 15:15:02 +0900

    refactor: NotPostOwnerException 메시지 변경

--- src/main/java/com/wootecobook/turkey/post/service/exception/NotPostOwnerException.java
+++ src/main/java/com/wootecobook/turkey/post/service/exception/NotPostOwnerException.java
@@ -2,9 +2,9 @@ package com.wootecobook.turkey.post.service.exception;
 
 public class NotPostOwnerException extends RuntimeException {
 
-    private static final String NOT_POST_OWENER_ERROR_MESSAGE = "삭제 권한이 없는 사용자 입니다.";
+    private static final String NOT_POST_OWNER_ERROR_MESSAGE = "권한이 없는 사용자 입니다.";
 
     public NotPostOwnerException() {
-        super(NOT_POST_OWENER_ERROR_MESSAGE);
+        super(NOT_POST_OWNER_ERROR_MESSAGE);
     }
 }
