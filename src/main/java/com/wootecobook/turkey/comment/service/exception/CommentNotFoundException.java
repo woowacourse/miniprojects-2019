9921adebc387401f79c5fe5850c2fb4972a0618a
DaejunBae <dpudpu11@gmail.com>
2019-08-16 13:39:23 +0900

    refactor: 모든 Exception log 제거

--- src/main/java/com/wootecobook/turkey/comment/service/exception/CommentNotFoundException.java
+++ src/main/java/com/wootecobook/turkey/comment/service/exception/CommentNotFoundException.java
@@ -4,11 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CommentNotFoundException extends RuntimeException {
-    private static final Logger log = LoggerFactory.getLogger(CommentNotFoundException.class);
     public static final String DEFAULT_MESSAGE = "해당 댓글을 찾을 수 없습니다.";
 
     public CommentNotFoundException(final Long id) {
         super(DEFAULT_MESSAGE + id);
-        log.error("id : {}", id);
     }
 }
--- src/main/java/com/wootecobook/turkey/comment/service/exception/CommentSaveException.java
+++ src/main/java/com/wootecobook/turkey/comment/service/exception/CommentSaveException.java
@@ -4,11 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CommentSaveException extends RuntimeException {
-    private static final Logger log = LoggerFactory.getLogger(CommentSaveException.class);
     public static final String DEFAULT_MESSAGE = "해당 댓글을 저장할 수 없습니다.";
 
     public CommentSaveException(final String message) {
         super(DEFAULT_MESSAGE + message);
-        log.error(message);
     }
 }
