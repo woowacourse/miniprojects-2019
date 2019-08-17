7f8714bdffc177a7a98a272ffb71fd212240ebf6
ep1stas1s <masloz@konkuk.ac.kr>
2019-08-17 11:30:40 +0900

    refactor: http connection 중복 제거

--- src/main/java/com/woowacourse/dsgram/service/oauth/GithubClient.java
+++ src/main/java/com/woowacourse/dsgram/service/oauth/GithubClient.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -16,7 +15,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Component
 @Getter
@@ -37,34 +35,18 @@ public class GithubClient {
     private String userEmailUrl;
 
     public String getToken(String code) {
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) new URL(tokenUrl).openConnection();
+        UrlConnector<String> template = connection -> {
             connection.setRequestMethod("POST");
             connection.setDoOutput(true);
+            setUpBody(connection, code);
 
-            // TODO: 2019-08-16 MediaType.APPLICATION_JSON_UTF8_VALUE
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.addRequestProperty("Accept", "application/json");
-
-            setBody(code, connection);
-
-            if (connection.getResponseCode() == HttpStatus.OK.value()) {
-                return getJsonElement(connection)
-                        .getAsJsonObject().get("access_token").getAsString();
-            }
-
-        } catch (IOException e) {
-            log.error("Fail! - Token 받아오기 실패", e);
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-        throw new RuntimeException("연결 실패?");
+            return getJsonElement(connection)
+                    .getAsJsonObject().get("access_token").getAsString();
+        };
+        return template.connectHttpUrl(tokenUrl);
     }
 
-    private void setBody(String code, HttpURLConnection connection) {
+    private void setUpBody(HttpURLConnection connection, String code) {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty(CLIENT_ID, clientId);
         jsonObject.addProperty(CLIENT_SECRET, clientSecret);
@@ -80,48 +62,37 @@ public class GithubClient {
 
     private JsonElement getJsonElement(HttpURLConnection connection) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            String line = "";
             StringBuilder result = new StringBuilder();
 
+            String line;
             while ((line = br.readLine()) != null) {
                 result.append(line);
             }
 
             return new JsonParser().parse(result.toString());
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("뭘 적어야하지...?", e);
         }
-        throw new RuntimeException("뭘 적어야하지...?");
     }
 
     public String getUserEmail(String accessToken) {
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) new URL(userEmailUrl).openConnection();
+        UrlConnector<String> template = connection -> {
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("Authorization", "token " + accessToken);
-            connection.addRequestProperty("Accept", "application/json");
+            connection.addRequestProperty("Authorization", String.format("token %s", accessToken));
 
             return getJsonElement(connection).getAsJsonArray().get(0)
                     .getAsJsonObject().get("email").getAsString();
-        } catch (IOException e) {
-            log.error("Fail! - Email 가져오기", e);
-        }
-        throw new RuntimeException("뭘 적어야하지...?");
+        };
+        return template.connectHttpUrl(userEmailUrl);
     }
 
     public JsonElement getUserInformation(String accessToken) {
-        HttpURLConnection connection = null;
-        try {
-            connection = (HttpURLConnection) new URL(userInfoUrl).openConnection();
+        UrlConnector<JsonElement> template = connection -> {
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("Authorization", String.format("token %s", accessToken));
-            connection.addRequestProperty("Accept", "application/json");
+            connection.addRequestProperty("Authorization", String.format("token %s", accessToken));
             return getJsonElement(connection);
-        } catch (IOException e) {
-            log.error("Fail! - User Information 가져오기", e);
-        }
-        throw new RuntimeException("뭘 적어야하지...?");
+        };
+        return template.connectHttpUrl(userInfoUrl);
     }
 }
 
