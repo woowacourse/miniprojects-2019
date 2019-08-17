019a03ef83ca5e44c6245bb688d932850ff68258
Soojin Roh <sooreal91@gmail.com>
2019-08-17 17:02:53 +0900

    refactor: ArticleApiService의 DI주입 방식 변경 및 JPA exception 발생 메서드 수정
    1. 필드 주입으로 DI 받던 ArticleRepository를 생성자 주입 방식으로 변경
    2. create() 메소드에서 Exception을 catch해서 JPA Exception을 handling하던 것에서 DataAccessException을 catch하는 것으로 수정

--- src/main/java/com/woowacourse/dsgram/service/ArticleApiService.java
+++ src/main/java/com/woowacourse/dsgram/service/ArticleApiService.java
@@ -4,19 +4,23 @@ import com.woowacourse.dsgram.domain.Article;
 import com.woowacourse.dsgram.domain.ArticleRepositoy;
 import com.woowacourse.dsgram.service.exception.JpaException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ArticleApiService {
 
-    @Autowired
     ArticleRepositoy articleRepositoy;
+
+    public ArticleApiService(ArticleRepositoy articleRepository) {
+        this.articleRepositoy = articleRepository;
+    }
 
     public Article create(Article article) {
         try {
             return articleRepositoy.save(article);
-        } catch (Exception exception) {
+        } catch (DataAccessException exception) {
             throw new JpaException("게시글 생성에 실패하였습니다.", exception);
         }
     }
