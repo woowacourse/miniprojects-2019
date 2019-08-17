ac712bbf3f5dd56016bbd97980d008275f85719c
Seongmo Jeon <oeeen3@gmail.com>
2019-08-17 14:10:03 +0900

    refactor: responseEntity 적용

--- src/main/java/com/woowacourse/sunbook/presentation/ArticleController.java
+++ src/main/java/com/woowacourse/sunbook/presentation/ArticleController.java
@@ -4,6 +4,8 @@ import com.woowacourse.sunbook.application.ArticleService;
 import com.woowacourse.sunbook.application.dto.article.ArticleResponseDto;
 import com.woowacourse.sunbook.domain.article.ArticleFeature;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,22 +21,30 @@ public class ArticleController {
     }
 
     @GetMapping
-    public List<ArticleResponseDto> show() {
-        return articleService.findAll();
+    public ResponseEntity<List<ArticleResponseDto>> show() {
+        List<ArticleResponseDto> articles = articleService.findAll();
+
+        return new ResponseEntity<>(articles, HttpStatus.OK);
     }
 
     @PostMapping
-    public ArticleResponseDto save(@RequestBody ArticleFeature articleFeature) {
-        return articleService.save(articleFeature);
+    public ResponseEntity<ArticleResponseDto> save(@RequestBody ArticleFeature articleFeature) {
+        ArticleResponseDto articleResponseDto = articleService.save(articleFeature);
+
+        return new ResponseEntity<>(articleResponseDto, HttpStatus.OK);
     }
 
     @PutMapping("/{articleId}")
-    public ArticleResponseDto modify(@PathVariable long articleId, @RequestBody ArticleFeature articleFeature) {
-        return articleService.modify(articleId, articleFeature);
+    public ResponseEntity<ArticleResponseDto> modify(@PathVariable long articleId, @RequestBody ArticleFeature articleFeature) {
+        ArticleResponseDto articleResponseDto = articleService.modify(articleId, articleFeature);
+
+        return new ResponseEntity<>(articleResponseDto, HttpStatus.OK);
     }
 
     @DeleteMapping("/{articleId}")
-    public void remove(@PathVariable long articleId) {
+    public ResponseEntity<Void> remove(@PathVariable long articleId) {
         articleService.remove(articleId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
