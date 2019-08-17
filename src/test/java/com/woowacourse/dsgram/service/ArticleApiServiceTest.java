beeb887421ea07bac3560f2c78a4cd10a8536d44
Soojin Roh <sooreal91@gmail.com>
2019-08-17 16:54:15 +0900

    test: ArticleApiServiceTest 코드 추가
    1. 게시글 작성 성공 테스트 코드 추가
    2. 게시글 조회 성공 테스트 코드 추가
    
    각각의 실패 테스트 코드 추가 필요

---
+++ src/test/java/com/woowacourse/dsgram/service/ArticleApiServiceTest.java
@@ -0,0 +1,65 @@
+package com.woowacourse.dsgram.service;
+
+import com.woowacourse.dsgram.domain.Article;
+import com.woowacourse.dsgram.domain.ArticleRepositoy;
+import com.woowacourse.dsgram.service.exception.JpaException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+public class ArticleApiServiceTest {
+
+    private Article article;
+
+    @InjectMocks
+    private ArticleApiService articleApiService;
+
+    @Mock
+    ArticleRepositoy articleRepositoy;
+
+
+    @BeforeEach
+    void setUp() {
+        article = new Article("contents", "fileName", "filePath");
+    }
+
+    @Test
+    void 게시글_생성_성공() {
+        given(articleRepositoy.save(article)).willReturn(article);
+
+        articleApiService.create(article);
+
+        verify(articleRepositoy).save(article);
+    }
+
+    @Test
+    void 게시글_생성_실패() {
+        // todo : 게시글 생성 실패 케이스 논의 필요
+    }
+
+    @Test
+    void 게시글_조회_성공() {
+        given(articleRepositoy.findById(any())).willReturn(Optional.of(article));
+        articleApiService.findById(1L);
+        verify(articleRepositoy).findById(anyLong());
+    }
+
+    @Test
+    void 게시글_조회_실패() {
+        // todo : article을 create했으면 테스트가 실패해야 되는데 성공함..
+        articleApiService.create(article);
+        assertThrows(JpaException.class, () -> articleApiService.findById(1L));
+    }
+}
