ecaf16d82c7eb2b1da23fb0d07c4dac378c6b4ca
starkim06 <starkim06@naver.com>
2019-08-17 17:14:06 +0900

    [#57] refactor: Image 클래스에서 Http요청을 통해서 이미지 URL확인로직 제거

--- src/test/java/com/woowacourse/zzazanstagram/model/article/domain/ArticleTest.java
+++ src/test/java/com/woowacourse/zzazanstagram/model/article/domain/ArticleTest.java
@@ -5,14 +5,10 @@ import com.woowacourse.zzazanstagram.model.article.domain.vo.Image;
 import com.woowacourse.zzazanstagram.model.member.domain.Member;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.woowacourse.zzazanstagram.model.article.ArticleConstant.CONTENTS;
 import static com.woowacourse.zzazanstagram.model.article.ArticleConstant.IMAGE_URL;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ArticleTest {
     private Image image;
@@ -29,29 +25,13 @@ class ArticleTest {
                 .name("name")
                 .email("test@test.com")
                 .password("password1!")
-                .profile("https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-600w-1029171697.jpg")
+                .profile("https://image.shutterstock.com/image-photo/600w-1029171697.jpg")
                 .build();
         article = new Article(image, contents, author);
     }
 
     @Test
-    void create() {
+    void Article_객체_생성() {
         assertThat(article).isEqualTo(new Article(image, contents, author));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"sdfsdfsdf", "notUrl"})
-    void 이미지_url_비정상_체크(String imageUrl) {
-        assertThrows(IllegalArgumentException.class, () ->
-                new Article(Image.of(imageUrl), contents, author));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-600w-1029171697.jpg",
-            "https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-600w-1048185397.jpg"})
-    void 이미지_url_정상_체크(String imageUrl) {
-        assertThatCode(() ->
-                new Article(Image.of(imageUrl), contents, author))
-                .doesNotThrowAnyException();
     }
 }
