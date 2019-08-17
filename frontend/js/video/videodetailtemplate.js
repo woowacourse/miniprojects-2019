fba0191afe1b670efb02b91ae68cc759e2267dd5
heebong <heebg2@naver.com>
2019-08-17 17:03:24 +0900

    [refactor] change video detail markup (#56)

--- frontend/js/video/videodetailtemplate.js
+++ frontend/js/video/videodetailtemplate.js
@@ -1,32 +1,29 @@
 function addVideoDetailTemplate(data) {
-    const videoDetailTemplate = 
-    `<iframe width="560" height="315" src="https://www.youtube.com/embed/${data.youtubeId}"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen></iframe>
-    <form class="form-horizontal mrg-top-40 pdd-right-30">
-
+    const videoDetailTemplate =
+        `<iframe width="913" height="514"
+            src="https://www.youtube.com/embed/${data.youtubeId}" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+        <br><br>
         <div class="form-group row">
-            <div class="col-md-10">
-                <h1> ${data.title} </h1>
+            <div class="col-md-12">
+                <h1 class="text-bold">${data.title}</h1>
+                <h4>조회수 1,000회</h4>
             </div>
         </div>
+        <hr>
         <div class="form-group row">
+            <div class="col-md-2">
+                <img class="banner-owner-img img-circle relative"
+                    src="./images/default/eastjun_big.jpg" alt="">
+            </div>
             <div class="col-md-10">
-                ${data.contents}
+                <span class="text-bold">로비로비</span><br>
+                <span>게시일 : ${data.createDate}</span><br><br>
+                <span>${data.contents}</span>
             </div>
         </div>
-        <div class="form-group row">
-            <div class="col-md-10">
-                사람입니다.
-            </div>
-        </div>
-        <div class="form-group row">
-            <div class="col-md-10">
-                ${data.createDate}
-            </div>
-        </div>
-    </form>`
+        <hr>`
 
     const videoArea = document.getElementById('video-area')
     videoArea.insertAdjacentHTML('beforeend', videoDetailTemplate)
