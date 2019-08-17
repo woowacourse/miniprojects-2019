076db2c2b69ba9864b550a946b9040a9b9e1ec38
Soojin Roh <sooreal91@gmail.com>
2019-08-17 16:58:21 +0900

    refactor: 로컬의 파일을 읽어오는 기능을 ArticleController에서 FileService로 분리 및 이동
    ArticleController에서 로컬의 파일을 읽어와 byte[] base64로 변환하는 기능을 FileService의 readFile() 메소드로 분리 구현

--- src/main/java/com/woowacourse/dsgram/web/controller/ArticleController.java
+++ src/main/java/com/woowacourse/dsgram/web/controller/ArticleController.java
@@ -2,27 +2,21 @@ package com.woowacourse.dsgram.web.controller;
 
 import com.woowacourse.dsgram.domain.Article;
 import com.woowacourse.dsgram.service.ArticleApiService;
-import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Base64;
-
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
 
-    @Autowired
     private ArticleApiService articleApiService;
+
+    public ArticleController(ArticleApiService articleApiService) {
+        this.articleApiService = articleApiService;
+    }
 
     @GetMapping("/writing")
     public String moveToWritePage() {
@@ -34,16 +28,6 @@ public class ArticleController {
         Article article = articleApiService.findById(articleId);
         model.addAttribute("article", article);
         return "article";
-    }
-
-    @GetMapping("/{articleId}/file")
-    public ResponseEntity<byte[]> showArticleFile(@PathVariable long articleId) throws IOException {
-        Article article = articleApiService.findById(articleId);
-        File file = new File(article.getFilePath() +"/" + article.getFileName());
-        byte[] bytes = Files.readAllBytes(file.toPath());
-        byte[] base64 = Base64.getEncoder().encode(bytes);
-
-        return new ResponseEntity<>(base64, HttpStatus.OK);
     }
 
 }
