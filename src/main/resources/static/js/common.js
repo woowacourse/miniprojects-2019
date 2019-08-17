29ae3a88108fa82f9e0ad387b3950e6df8e1dcda
unknown <kmdngyu@gmail.com>
2019-08-17 21:34:43 +0900

    refactor: article layout

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,10 +1,2 @@
-document.querySelector('.search-toggle').addEventListener('click', toggleSearchInput)
-
-function toggleSearchInput(event) {
-    event.preventDefault();
-    document.querySelector('.search-box').classList.toggle('active')
-    document.querySelector(".search-input").classList.toggle("active")
-    document.querySelector(".search-input input").focus()
-}
 
 
