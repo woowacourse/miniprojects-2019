3ab638c6c3ecd598d30290b2d110eda0c3c172bc
이영현 <leey9494@icloud.com>
2019-08-19 16:42:03 +0900

    feat: add login interceptor

---
+++ src/main/java/techcourse/w3/woostagram/user/support/LoginConfig.java
@@ -0,0 +1,28 @@
+package techcourse.w3.woostagram.user.support;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import techcourse.w3.woostagram.user.interceptor.LoginInterceptor;
+
+@Configuration
+public class LoginConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+
+    public LoginConfig(LoginInterceptor loginInterceptor) {
+        this.loginInterceptor = loginInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/users/signup/form")
+                .excludePathPatterns("/users/signup")
+                .excludePathPatterns("/users/login")
+                .excludePathPatterns("/users/login/form")
+                .excludePathPatterns("/css/**")
+                .excludePathPatterns("/js/**")
+                .excludePathPatterns("/images/**");
+    }
+}
