8bd1ba2e7f557f30747085c116fd63e6fbd9330b
starkim06 <starkim06@naver.com>
2019-08-20 11:52:35 +0900

    [#74] feat: S3Uploader 클래스 추가

--- src/main/java/com/woowacourse/zzazanstagram/ZzazanstagramApplication.java
+++ src/main/java/com/woowacourse/zzazanstagram/ZzazanstagramApplication.java
@@ -1,13 +1,17 @@
 package com.woowacourse.zzazanstagram;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ZzazanstagramApplication {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(ZzazanstagramApplication.class, args);
+        new SpringApplicationBuilder(ZzazanstagramApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
-
 }
