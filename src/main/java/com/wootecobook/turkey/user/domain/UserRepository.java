b552cafee36615bb137a0bdd9f883b83c547a7fa
DaejunBae <dpudpu11@gmail.com>
2019-08-20 22:24:49 +0900

    feat: 이름으로 유저 검색

--- src/main/java/com/wootecobook/turkey/user/domain/UserRepository.java
+++ src/main/java/com/wootecobook/turkey/user/domain/UserRepository.java
@@ -1,9 +1,13 @@
 package com.wootecobook.turkey.user.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    List<User> findTop5ByNameIsContaining(@Param("name") String name);
 }
