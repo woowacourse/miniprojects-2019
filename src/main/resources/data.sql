d936b1fbed0961bcf40beee49548253d3d6d51b8
DaejunBae <dpudpu11@gmail.com>
2019-08-21 01:16:11 +0900

    feat: 이름으로 검색 프론트

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,10 @@
-INSERT INTO user (email, name, password)
-VALUES ('a@a.a', 'dpudpu', '1234');
+INSERT INTO user (email, name, password) VALUES ('a@a.a', 'dpudpu', '1234');
+INSERT INTO user (email, name, password) VALUES ('a@a.ab', 'dpudpud', '1234');
+INSERT INTO user (email, name, password) VALUES ('a@a.ac', 'dpudpua', '1234');
+INSERT INTO user (email, name, password) VALUES ('a@a.ad', 'dpudpux', '1234');
+INSERT INTO user (email, name, password) VALUES ('a@a.ae', 'fdpudpu', '1234');
+INSERT INTO user (email, name, password) VALUES ('a@a.af', 'edpudpu', '1234');
+INSERT INTO user (email, name, password) VALUES ('a@a.ah', 'dpudpu', '1234');
 
 INSERT INTO post (contents, user_id)
 VALUES ('contents', 1);
