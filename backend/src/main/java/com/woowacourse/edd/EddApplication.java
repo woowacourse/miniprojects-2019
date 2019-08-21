d73997c26d049d133f5902bc5452b7baaab205cf
Shin Jinwoo <bluesky0255@gmail.com>
2019-08-21 13:57:16 +0900

    [fix] fix issue by change JVM's default timezone (#111)
    * 서버에 관계없이 기본 타임존을 UTC로 하도록 설정

--- backend/src/main/java/com/woowacourse/edd/EddApplication.java
+++ backend/src/main/java/com/woowacourse/edd/EddApplication.java
@@ -3,8 +3,16 @@ package com.woowacourse.edd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EddApplication {
+
+    @PostConstruct
+    private void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EddApplication.class, args);
