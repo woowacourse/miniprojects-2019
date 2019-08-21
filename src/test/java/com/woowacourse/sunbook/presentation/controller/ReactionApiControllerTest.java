eeeeaf999e69c7c98f4e1e8184dfcc8ae0eb857e
CODEMCD <psbum1609@gmail.com>
2019-08-21 20:50:29 +0900

    fix: reaction controller 테스트 오류 수정

--- src/test/java/com/woowacourse/sunbook/presentation/controller/ReactionApiControllerTest.java
+++ src/test/java/com/woowacourse/sunbook/presentation/controller/ReactionApiControllerTest.java
@@ -2,6 +2,7 @@ package com.woowacourse.sunbook.presentation.controller;
 
 import com.woowacourse.sunbook.application.dto.reaction.ReactionDto;
 import com.woowacourse.sunbook.application.dto.user.UserRequestDto;
+import com.woowacourse.sunbook.domain.comment.CommentFeature;
 import com.woowacourse.sunbook.domain.user.UserEmail;
 import com.woowacourse.sunbook.domain.user.UserName;
 import com.woowacourse.sunbook.domain.user.UserPassword;
@@ -23,23 +24,10 @@ public class ReactionApiControllerTest extends TestTemplate {
     private UserRequestDto userLoginRequestDto = new UserRequestDto(userEmail, userName, userPassword);
     ReactionDto reactionDto = new ReactionDto(0L);
 
-
-    @BeforeEach
-    void setUp() {
-        // 로그인
-        // 유저 정보는 data.sql로 미리 넣어둠
-        respondApi(request(HttpMethod.POST, "/api/signin", userLoginRequestDto, HttpStatus.OK))
-                .jsonPath("$..email").isEqualTo(USER_EMAIL)
-                .jsonPath("$..name").isEqualTo(USER_NAME)
-                ;
-
-        // 게시글은 data.sql로 미리 넣어둠
-    }
-
     @Test
     void 좋아요_정상_동작() {
-        respondApi(request(HttpMethod.POST, "/api/articles/1/good",
-                reactionDto, HttpStatus.OK))
+        String sessionId = loginSessionId(userRequestDto);
+        respondApi(loginAndRequest(HttpMethod.POST, "/api/articles/1/good", reactionDto, HttpStatus.OK, sessionId))
                 .jsonPath("$.numberOfGood").isEqualTo(1L)
                 ;
     }
@@ -47,38 +35,33 @@ public class ReactionApiControllerTest extends TestTemplate {
     @Test
     void 초기_좋아요_개수_정상_조회() {
         // 좋아요 조회
-        respondApi(request(HttpMethod.GET, "/api/articles/2/good",
-                reactionDto, HttpStatus.OK))
+        String sessionId = loginSessionId(userRequestDto);
+        respondApi(loginAndRequest(HttpMethod.GET, "/api/articles/2/good", reactionDto, HttpStatus.OK, sessionId))
                 .jsonPath("$.numberOfGood").isEqualTo(0L)
                 ;
     }
 
     @Test
     void 좋아요_후_개수_정상_조회() {
-        // 좋아요 누르기(TestTemplate로 분리하기)
-        respondApi(request(HttpMethod.POST, "/api/articles/3/good",
-                reactionDto, HttpStatus.OK))
+        // 좋아요 누르기(TestTemplate 로 분리하기)
+        String sessionId = loginSessionId(userRequestDto);
+        respondApi(loginAndRequest(HttpMethod.POST, "/api/articles/3/good", reactionDto, HttpStatus.OK, sessionId))
                 .jsonPath("$.numberOfGood").isEqualTo(1L)
-                ;
+        ;
 
         // 좋아요 조회
-        respondApi(request(HttpMethod.GET, "/api/articles/3/good",
-                reactionDto, HttpStatus.OK))
+        respondApi(loginAndRequest(HttpMethod.GET, "/api/articles/3/good", reactionDto, HttpStatus.OK, sessionId))
                 .jsonPath("$.numberOfGood").isEqualTo(1L)
-                ;
+        ;
     }
 
     @Test
     void 좋아요_취소_정상() {
-        respondApi(request(HttpMethod.POST, "/api/articles/4/good",
-                reactionDto, HttpStatus.OK))
+        String sessionId = loginSessionId(userRequestDto);
+        respondApi(loginAndRequest(HttpMethod.POST, "/api/articles/4/good", reactionDto, HttpStatus.OK, sessionId))
                 .jsonPath("$.numberOfGood").isEqualTo(1L)
                 ;
-
-        ReactionDto savedGood = new ReactionDto(1L);
-
-        respondApi(request(HttpMethod.POST, "/api/articles/4/good",
-                savedGood, HttpStatus.OK))
+        respondApi(loginAndRequest(HttpMethod.POST, "/api/articles/4/good", reactionDto, HttpStatus.OK, sessionId))
                 .jsonPath("$.numberOfGood").isEqualTo(0L)
                 ;
     }
