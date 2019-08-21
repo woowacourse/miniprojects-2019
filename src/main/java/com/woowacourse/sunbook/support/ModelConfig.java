b88b74aad096ba8ddf61ab5044d4fa9cebd6bf11
ddu0422 <ddu0422@naver.com>
2019-08-21 17:51:32 +0900

    chore: 사용하지 않는 bean 제거

--- src/main/java/com/woowacourse/sunbook/support/ModelConfig.java
+++ src/main/java/com/woowacourse/sunbook/support/ModelConfig.java
@@ -1,31 +1,31 @@
-//package com.woowacourse.sunbook.support;
-//
-//import com.amazonaws.regions.Regions;
-//import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import org.modelmapper.ModelMapper;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
-//
-//@Configuration
-//public class ModelConfig {
-//
-//    @Bean
-//    public ModelMapper modelMapper() {
-//        ModelMapper modelMapper = new ModelMapper();
-//        modelMapper.getConfiguration()
-//                .setFieldAccessLevel(PRIVATE)
-//                .setFieldMatchingEnabled(true);
-//
-//        return modelMapper;
-//    }
-//
+package com.woowacourse.sunbook.support;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+
+@Configuration
+public class ModelConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(PRIVATE)
+                .setFieldMatchingEnabled(true);
+
+        return modelMapper;
+    }
+
 //    @Bean
 //    AmazonS3 amazonS3() {
 //        return  AmazonS3ClientBuilder.standard()
 //                .withRegion(Regions.AP_NORTHEAST_2)
 //                .build();
 //    }
-//}
+}
