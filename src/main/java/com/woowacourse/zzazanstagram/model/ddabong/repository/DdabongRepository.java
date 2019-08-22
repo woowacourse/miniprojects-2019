ec51e58204d7c02d76b9de6a393e1f4b623b7eb0
starkim06 <starkim06@naver.com>
2019-08-22 10:31:08 +0900

    [#98] refactor: @Repository 어노테이션 삭제

--- src/main/java/com/woowacourse/zzazanstagram/model/ddabong/repository/DdabongRepository.java
+++ src/main/java/com/woowacourse/zzazanstagram/model/ddabong/repository/DdabongRepository.java
@@ -4,9 +4,7 @@ import com.woowacourse.zzazanstagram.model.article.domain.Article;
 import com.woowacourse.zzazanstagram.model.ddabong.domain.Ddabong;
 import com.woowacourse.zzazanstagram.model.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DdabongRepository extends JpaRepository<Ddabong, Long> {
     Ddabong findByArticleAndMember(Article article, Member member);
 }
