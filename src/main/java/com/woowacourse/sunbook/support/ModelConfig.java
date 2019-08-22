4145fa83d76287d60787a00a9efb16395b4b26ad
CODEMCD <psbum1609@gmail.com>
2019-08-22 16:54:40 +0900

    refactor: 주석 제거

--- src/main/java/com/woowacourse/sunbook/support/ModelConfig.java
+++ src/main/java/com/woowacourse/sunbook/support/ModelConfig.java
@@ -1,8 +1,5 @@
 package com.woowacourse.sunbook.support;
 
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,11 +18,4 @@ public class ModelConfig {
 
         return modelMapper;
     }
-
-//    @Bean
-//    AmazonS3 amazonS3() {
-//        return  AmazonS3ClientBuilder.standard()
-//                .withRegion(Regions.AP_NORTHEAST_2)
-//                .build();
-//    }
 }
