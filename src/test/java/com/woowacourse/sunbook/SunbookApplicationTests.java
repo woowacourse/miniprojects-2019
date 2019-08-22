fe10bfc6dc76eb57c8a87a6d5d9055dc5fefcbcc
CODEMCD <psbum1609@gmail.com>
2019-08-22 16:55:59 +0900

    refactor: 사용하지 않는 어노테이션 제거

--- src/test/java/com/woowacourse/sunbook/SunbookApplicationTests.java
+++ src/test/java/com/woowacourse/sunbook/SunbookApplicationTests.java
@@ -1,13 +1,10 @@
 package com.woowacourse.sunbook;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ActiveProfiles("test")
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SunbookApplicationTests {
 
