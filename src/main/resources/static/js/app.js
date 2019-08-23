d7e4e8d9962e532a29e9102e46ef6d3d198b04f9
Doughnut <48856468+codeanddonuts@users.noreply.github.com>
2019-08-23 15:31:04 +0900

    refactor: front-end scripts (#98)
    * refactor: front-end scripts
    
    * fix: article editing

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,21 +2,42 @@ const App = (() => {
   "use strict"
 
   const BASE_URL = "http://" + window.location.host
-  const ENTER = 13
-  const MINUTE = 1000 * 60
-  const HOUR = 60 * MINUTE
-  const DAY = 24 * HOUR
-  const WEEK = 7 * DAY
+  class Api {
+    get(path) {
+      return axios.get(BASE_URL + path)
+    }
+
+    post(path, data) {
+      return axios.post(BASE_URL + path, data)
+    }
+
+    put(path, data) {
+      return axios.post(BASE_URL + path, data)
+    }
+
+    delete(path) {
+      return axios.delete(BASE_URL + path)
+    }
+  }
+
   class Service {
-    constructor() {
+    constructor(api) {
+      this.api = api
       this.editBackup = {}
     }
 
     isEnterKey(e) {
+      const ENTER = 13
+
       return e.keyCode === ENTER || e.which === ENTER || e.key === ENTER
     }
 
     formatDate(dateString) {
+      const MINUTE = 1000 * 60
+      const HOUR = 60 * MINUTE
+      const DAY = 24 * HOUR
+      const WEEK = 7 * DAY
+
       const date = new Date(dateString)
       const difference = (new Date()).getTime() - date.getTime()
       if (difference < 10 * MINUTE) {
@@ -35,78 +56,6 @@ const App = (() => {
     }
   }
 
-  const ARTICLE_TEMPLATE_HTML =
-    '<div id="article-{{id}}" class="card widget-feed padding-15">' +
-      '<div class="feed-header">' +
-        '<ul class="list-unstyled list-info">' +
-          '<li>' +
-            '<img class="thumb-img img-circle" src="/images/profile/{{user.coverUrl}}" alt="{{user.name}}">' +
-            '<div class="info">' +
-              '<a href="/users/{{user.id}}" class="title no-pdd-vertical text-semibold inline-block">{{user.name}}</a>' +
-              '<span>님이 게시물을 작성하였습니다.</span>' +
-              '<span class="sub-title">{{date}}</span>' +
-              '<a class="pointer absolute top-0 right-0" data-toggle="dropdown" aria-expanded="false">' +
-                '<span class="btn-icon text-dark">' +
-                  '<i class="ti-more font-size-16"></i>' +
-                '</span>' +
-              '</a>' +
-              '<ul class="dropdown-menu">' +
-                '<li>' +
-                  '<a href="javascript:App.editArticle({{id}})" class="pointer">' +
-                    '<i class="ti-pencil pdd-right-10 text-dark"></i>' +
-                    '<span>게시글 수정</span>' +
-                  '</a>' +
-                  '<a href="javascript:App.removeArticle({{id}})" class="pointer">' +
-                    '<i class="ti-trash pdd-right-10 text-dark"></i>' +
-                    '<span>게시글 삭제</span>' +
-                  '</a>' +
-                '</li>' +
-              '</ul>' +
-            '</div>' +
-          '</li>' +
-        '</ul>' +
-      '</div>' +
-      '<div id="article-{{id}}-content" class="feed-body no-pdd">' +
-        '<p>' +
-          '<span> {{content}} </span>' +
-        '</p>' +
-        '{{#each image}}' +
-      '<img class="vertical-align" src="/{{this.path}}">' +
-        '{{/each}}' +
-      '</div>' +
-      '<ul class="feed-action pdd-btm-5 border bottom">' +
-        '<li>' +
-          '<i class="fa fa-thumbs-o-up text-info font-size-16 mrg-left-5"></i>' +
-          '<span id="count-of-like-{{id}}" class="font-size-14 lh-2-1"> 0</span>' +
-        '</li>' +
-        '<li class="float-right mrg-right-15">' +
-          '<span class="font-size-13">댓글 <span id="count-of-comment-{{id}}">0</span>개</span>' +
-        '</li>' +
-      '</ul>' +
-      '<ul class="feed-action border bottom d-flex">' +
-        '<li class="text-center flex-grow-1">' +
-          '<button id="article-like-{{id}}" onclick="App.likeArticle({{id}})" class="btn btn-default no-border pdd-vertical-0 no-mrg width-100">' +
-            '<i class="fa fa-thumbs-o-up font-size-16"></i>' +
-            '<span class="font-size-13"> 좋아요</span>' +
-          '</button>' +
-        '</li>' +
-        '<li class="text-center flex-grow-1">' +
-          '<button class="btn btn-default no-border pdd-vertical-0 no-mrg width-100">' +
-            '<i class="fa fa-comment-o font-size-16"></i>' +
-            '<span class="font-size-13"> 댓글</span>' +
-          '</button>' +
-        '</li>' +
-      '</ul>' +
-      '<div class="feed-footer">' +
-        '<div class="comment">' +
-        '<ul id="comments-{{id}}" class="list-unstyled list-info"></ul>' +
-          '<div class="add-comment">' +
-            '<textarea id="new-comment-{{id}}" rows="1" class="form-control" placeholder="댓글을 입력하세요." onkeydown="App.writeComment(event, {{id}})"></textarea>' +
-          '</div>' +
-        '</div>' +
-      '</div>' +
-    '</div>'
-  const articleTemplate = Handlebars.compile(ARTICLE_TEMPLATE_HTML)
   class ArticleService extends Service {
     async write() {
       const textbox = document.getElementById("new-article")
@@ -123,22 +72,22 @@ const App = (() => {
           textbox.value = ""
           document.getElementById("articles").insertAdjacentHTML(
             "afterbegin",
-            articleTemplate({
+            templates.articleTemplate({
               "id": article.id,
               "content": article.content,
               "date": super.formatDate(article.recentDate),
               "user": article.userOutline,
-              "image": article.attachments
+              "images": article.attachments
             })
           )
-          document.getElementById("attachment").value=""
+          document.getElementById("attachment").value = ""
         } catch (e) {}
       }
     }
 
     edit(id) {
       const contentArea = document.getElementById("article-" + id + "-content")
-      const originalContent = contentArea.firstChild.firstChild.innerHTML.trim()
+      const originalContent = contentArea.firstElementChild.firstElementChild.innerText
       contentArea.innerHTML = ""
       contentArea.insertAdjacentHTML(
         "afterbegin",
@@ -162,7 +111,7 @@ const App = (() => {
           }
         })()
         contentArea.innerHTML = ""
-        contentArea.insertAdjacentHTML("afterbegin", "<p><span> " + result + " </span></p>")
+        contentArea.insertAdjacentHTML("afterbegin", "<p><span> " + templates.escapeHtml(result) + " </span></p>")
         super.editBackup[id] = undefined
       }
     }
@@ -179,29 +128,11 @@ const App = (() => {
         await axios.post(BASE_URL + "/api/articles/" + id + "/like")
         const likeButton = document.getElementById("article-like-" + id)
         likeButton.classList.toggle('liked')
-
-        const countOfLike = (await axios.get(BASE_URL + "/api/articles/" + id + "/like/count")).data
-        document.getElementById("count-of-like-" + id).innerText = " " + countOfLike
+        document.getElementById("count-of-like-" + id).innerText = " " + (await axios.get(BASE_URL + "/api/articles/" + id + "/like/count")).data
       } catch (e) {}
     }
   }
-
-  const COMMENT_TEMPLATE_HTML =
-    '<li class="comment-item">' +
-      '<img class="thumb-img img-circle" src="/images/profile/{{user.coverUrl}}" alt="{{user.name}}">' +
-      '<div class="info">' +
-        '<div class="bg-lightgray border-radius-18 padding-10 max-width-100">' +
-          '<a href="/users/{{user.id}}" class="title text-bold inline-block text-link-color">{{user.name}}</a>' +
-          '<span> {{content}}</span>' +
-        '</div>' +
-        '<div class="font-size-12 pdd-left-10 pdd-top-5">' +
-          '<span class="pointer text-link-color">좋아요</span>' +
-          '<span> · </span>' +
-          '<span>{{date}}</span>' +
-        '</div>' +
-      '</div>' +
-    '</li>'
-  const commentTemplate = Handlebars.compile(COMMENT_TEMPLATE_HTML)
+  
   class CommentService extends Service {
     async write(event, id) {
       event = event || window.event
@@ -215,16 +146,14 @@ const App = (() => {
           textbox.value = ""
           document.getElementById("comments-" + id).insertAdjacentHTML(
             "beforeend",
-            commentTemplate({
+            templates.commentTemplate({
               "id": comment.id,
               "content": comment.content,
               "date": super.formatDate(comment.createdDate),
               "user": comment.userOutline
             })
           )
-
-          const countOfComment = (await axios.get(BASE_URL + "/api/articles/" + id + "/comments/count")).data
-          document.getElementById("count-of-comment-" + id).innerText = countOfComment
+          document.getElementById("count-of-comment-" + id).innerText = (await axios.get(BASE_URL + "/api/articles/" + id + "/comments/count")).data
         } catch (e) {}
       }
     }
@@ -233,9 +162,7 @@ const App = (() => {
       try {
         await axios.delete(BASE_URL + "/api/comments/" + id)
         document.getElementById("comments-" + id).remove()
-
-        const countOfComment = (await axios.get(BASE_URL + "/api/articles/" + id + "/comments/count")).data
-        document.getElementById("count-of-comment-" + id).innerText = countOfComment
+        document.getElementById("count-of-comment-" + id).innerText = (await axios.get(BASE_URL + "/api/articles/" + id + "/comments/count")).data
       } catch (e) {}
     }
   }
@@ -303,5 +230,6 @@ const App = (() => {
     }
   }
 
-  return new Controller(new ArticleService(), new CommentService(), new FriendService())
+  const api = new Api()
+  return new Controller(new ArticleService(api), new CommentService(api), new FriendService(api))
 })()
---
+++ src/main/resources/static/js/templates.js
@@ -0,0 +1,104 @@
+const templates = (() => {
+  class Templates {
+    escapeHtml(string) {
+      return string.replace("<", "&lt;").replace(">", "&gt;").replace("\n", "<br>")
+    }
+
+    articleTemplate(input) {
+      const ARTICLE_TEMPLATE_HTML_A =
+        `<div id="article-${input.id}" class="card widget-feed padding-15">
+          <div class="feed-header">
+            <ul class="list-unstyled list-info">
+              <li>
+                <img class="thumb-img img-circle" src="/images/profile/${input.user.coverUrl}" alt="${input.user.name}">
+                <div class="info">
+                  <a href="/users/${input.user.id}" class="title no-pdd-vertical text-semibold inline-block">${input.user.name}</a>
+                  <span>님이 게시물을 작성하였습니다.</span>
+                  <span class="sub-title">${input.date}</span>
+                  <a class="pointer absolute top-0 right-0" data-toggle="dropdown" aria-expanded="false">
+                    <span class="btn-icon text-dark">
+                      <i class="ti-more font-size-16"></i>
+                    </span>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a href="javascript:App.editArticle(${input.id})" class="pointer">
+                        <i class="ti-pencil pdd-right-10 text-dark"></i>
+                        <span>게시글 수정</span>
+                      </a>
+                      <a href="javascript:App.removeArticle(${input.id})" class="pointer">
+                        <i class="ti-trash pdd-right-10 text-dark"></i>
+                        <span>게시글 삭제</span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div id="article-${input.id}-content" class="feed-body no-pdd">
+            <p>
+              <span> ${this.escapeHtml(input.content)} </span> 
+            </p>`
+      const ARTICLE_TEMPLATE_HTML_B =
+          `</div>
+          <ul class="feed-action pdd-btm-5 border bottom">
+            <li>
+              <i class="fa fa-thumbs-o-up text-info font-size-16 mrg-left-5"></i>
+              <span id="count-of-like-${input.id}" class="font-size-14 lh-2-1"> 0</span>
+            </li>
+            <li class="float-right mrg-right-15">
+              <span class="font-size-13">댓글 <span id="count-of-comment-${input.id}">0</span>개</span>
+            </li>
+          </ul>
+          <ul class="feed-action border bottom d-flex">
+            <li class="text-center flex-grow-1">
+              <button id="article-like-${input.id}" onclick="App.likeArticle(${input.id})" class="btn btn-default no-border pdd-vertical-0 no-mrg width-100">
+                <i class="fa fa-thumbs-o-up font-size-16"></i>
+                <span class="font-size-13"> 좋아요</span>
+              </button>
+            </li>
+            <li class="text-center flex-grow-1">
+              <button class="btn btn-default no-border pdd-vertical-0 no-mrg width-100">
+                <i class="fa fa-comment-o font-size-16"></i>
+                <span class="font-size-13"> 댓글</span>
+              </button>
+            </li>
+          </ul>
+          <div class="feed-footer">
+            <div class="comment">
+            <ul id="comments-${input.id}" class="list-unstyled list-info"></ul>
+              <div class="add-comment">
+                <textarea id="new-comment-${input.id}" rows="1" class="form-control" placeholder="댓글을 입력하세요." onkeydown="App.writeComment(event, ${input.id})"></textarea>
+              </div>
+            </div>
+          </div>
+        </div>`
+
+      let result = ARTICLE_TEMPLATE_HTML_A
+      input.images.forEach(image => {
+        result += `<img class="vertical-align" src="/${image.path}">`
+      })
+      return result + ARTICLE_TEMPLATE_HTML_B
+    }
+
+    commentTemplate(input) {
+        return `<li class="comment-item">
+          <img class="thumb-img img-circle" src="/images/profile/${input.user.coverUrl}" alt="${input.user.name}">
+          <div class="info">
+            <div class="bg-lightgray border-radius-18 padding-10 max-width-100">
+              <a href="/users/${input.user.id}" class="title text-bold inline-block text-link-color">${input.user.name}</a>
+              <span> ${this.escapeHtml(input.content)}</span>
+            </div>
+            <div class="font-size-12 pdd-left-10 pdd-top-5">
+              <span class="pointer text-link-color">좋아요</span>
+              <span> · </span>
+              <span>${input.date}</span>
+            </div>
+          </div>
+        </li>`
+    }
+  }
+
+  return new Templates()
+})()
