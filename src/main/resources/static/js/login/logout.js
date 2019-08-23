757672461a3eb08cd862c81b81c1b0286c74e1b4
skygl <jupiter9501@gmal.com>
2019-08-23 14:50:33 +0900

    refactor: 프론트 피드백 수정
    - let -> const 수정
    - jQuery ajax 에서 api.js의 Fetch API 사용

--- src/main/resources/static/js/login/logout.js
+++ src/main/resources/static/js/login/logout.js
@@ -1,19 +1,15 @@
-function logout() {
+async function logout() {
 	const logoutConfirm = confirm('로그아웃 하시겠습니까?')
 
 	if(logoutConfirm) {
-        $.ajax({
-            url:"/logout",
-            type:"POST",
-            async:false,
-            dataType:'text',
-            error:function(request,status,error){
-            	alert('로그인 상태가 아닙니다.')
-            },
-            success:function(data){
-            	alert('로그아웃 되었습니다.')
-            }
-        })
+        const res = await api.POST("/logout")
+
+        if (res.status == 200) {
+            alert('로그아웃 되었습니다.')
+        }
+        else {
+            alert('로그인 상태가 아닙니다.')
+        }
 
         window.location.href="/users/form"
 	}
