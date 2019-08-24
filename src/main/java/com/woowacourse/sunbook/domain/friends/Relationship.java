0b8be8f5beaabc677524c7d8ed20c29c9021e3d2
hyun <jhs695@naver.com>
2019-08-24 18:46:26 +0900

    feat: Relation Enum 추가

---
+++ src/main/java/com/woowacourse/sunbook/domain/friends/Relationship.java
@@ -0,0 +1,31 @@
+package com.woowacourse.sunbook.domain.friends;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public enum Relationship {
+	FRIEND("FRIEND"),
+	ADD("ADD"),
+	REQUESTED("REQUESTED"),
+	NONE("NONE");
+
+	private String relationship;
+
+	Relationship(String relationship) {
+		this.relationship = relationship;
+	}
+
+	public boolean isPossibleAddOrRequested() {
+		return this == NONE;
+	}
+
+	public boolean isPossibleBeFriend() {
+		return this == ADD || this == REQUESTED;
+	}
+
+	public boolean isPossibleRemove() {
+		return this == ADD || this == REQUESTED || this == FRIEND;
+	}
+}
