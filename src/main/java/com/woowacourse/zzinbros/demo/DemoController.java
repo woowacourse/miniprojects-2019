a920e149e6b0e72793c320a2636ea64ba1086b9f
Deocksoo <38533611+Deocksoo@users.noreply.github.com>
2019-08-26 13:59:44 +0900

    refactor(front): Login area와 signup area의 중복되는 tag id 수정 (#167)
    * refactor: 사용하지 않는 template 제거
    
    1. login, signup 페이지는 entrance 페이지로 이동되어 제거합니다.
    2. userpage와 관련된 템플릿들을 디렉토리를 생성하여 모두 이동시켰는데, 머지 과정에서
    다시 섞여들어온 파일들이 보입니다. 정리되지 않은 해당 템플릿 파일들을 다시 삭제하였습니다.
    
    * refactor: sign-up area와 login-area의 중복된 tag id 변경
    
    두 템플릿을 합치는 과정에서 email과 password의 input tag id가 중복되는 버그가 있었습니다.
    이를 수정합니다.
    
    * refactor(front): entrance 페이지 왼쪽 logo 크기 수정
    
    가입 신청서 작성 템플릿 사이즈에 맞추어 크기를 수정합니다.

--- src/main/java/com/woowacourse/zzinbros/demo/DemoController.java
+++ src/main/java/com/woowacourse/zzinbros/demo/DemoController.java
@@ -23,16 +23,6 @@ public class DemoController {
         return "index";
     }
 
-    @GetMapping("/signup")
-    public String signup() {
-        return "signup";
-    }
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
     @GetMapping("/entrance")
     public String enter() {
         return "entrance";
