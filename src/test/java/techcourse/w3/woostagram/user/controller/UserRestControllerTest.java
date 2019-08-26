556839c10401ef16251d3bbc64add7dba23ce185
김성훈 <seonghun127@gmail.com>
2019-08-26 17:26:44 +0900

    feat: test code

--- src/test/java/techcourse/w3/woostagram/user/controller/UserRestControllerTest.java
+++ src/test/java/techcourse/w3/woostagram/user/controller/UserRestControllerTest.java
@@ -1,11 +1,33 @@
 package techcourse.w3.woostagram.user.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import techcourse.w3.woostagram.AbstractControllerTests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserRestControllerTest extends AbstractControllerTests {
+
+    @Test
+    void uploadProfileImage_correctProfileImage_isOk() {
+        MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
+        bodyBuilder.part("imageFile", new ByteArrayResource("<<png data>>".getBytes()) {
+            @Override
+            public String getFilename() {
+                return "test_image.jpg";
+            }
+        }, MediaType.IMAGE_JPEG);
+
+        assertThat(postMultipartRequest("/api/users", bodyBuilder.build()).getStatus().is2xxSuccessful()).isTrue();
+    }
+
+    @Test
+    void readLoginInformation_correctLoggedInUserId_isOk() {
+        assertThat(getRequest("/api/users/loggedin").getStatus().is2xxSuccessful()).isTrue();
+    }
+
     @Test
     void delete_correct_isOk() {
         assertThat(deleteRequest("api/users").getStatus().is2xxSuccessful()).isTrue();
