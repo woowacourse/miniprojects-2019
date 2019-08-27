512255f96e7637d9c23e76fbc30271af2acec281
김성훈 <seonghun127@gmail.com>
2019-08-27 17:43:27 +0900

    feat: add user exception handler

---
+++ src/main/java/techcourse/w3/woostagram/user/UserRestExceptionHandler.java
@@ -0,0 +1,55 @@
+package techcourse.w3.woostagram.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import techcourse.w3.woostagram.article.exception.FileSaveFailException;
+import techcourse.w3.woostagram.article.exception.InvalidExtensionException;
+import techcourse.w3.woostagram.user.dto.UserErrorDto;
+import techcourse.w3.woostagram.user.exception.UserNotFoundException;
+import techcourse.w3.woostagram.user.exception.UserProfileException;
+
+@Slf4j
+@ResponseBody
+@ControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UserRestExceptionHandler {
+    @ExceptionHandler(UserProfileException.class)
+    public UserErrorDto handleUserProfileException(UserProfileException error) {
+        log.debug("errorMessage: {}", error.getMessage());
+        UserErrorDto userErrorDto = new UserErrorDto();
+        userErrorDto.setErrorMessage(error.getMessage());
+        userErrorDto.setErrorCode("not selected file");
+        return userErrorDto;
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public UserErrorDto handleUserNotFoundException(UserNotFoundException error) {
+        log.debug("errorMessage: {}", error.getMessage());
+        UserErrorDto userErrorDto = new UserErrorDto();
+        userErrorDto.setErrorMessage(error.getMessage());
+        userErrorDto.setErrorCode("not exist user");
+        return userErrorDto;
+    }
+
+    @ExceptionHandler(InvalidExtensionException.class)
+    public UserErrorDto handleInvalidExtensionException(InvalidExtensionException error) {
+        log.debug("errorMessage: {}", error.getMessage());
+        UserErrorDto userErrorDto = new UserErrorDto();
+        userErrorDto.setErrorMessage(error.getMessage());
+        userErrorDto.setErrorCode("not support file extension");
+        return userErrorDto;
+    }
+
+    @ExceptionHandler(FileSaveFailException.class)
+    public UserErrorDto handleFileSaveFailException(FileSaveFailException error) {
+        log.debug("errorMessage: {}", error.getMessage());
+        UserErrorDto userErrorDto = new UserErrorDto();
+        userErrorDto.setErrorMessage(error.getMessage());
+        userErrorDto.setErrorCode("fail uploading image file");
+        return userErrorDto;
+    }
+}
