71006a71b6269f7c02f31dea389ef4124d4f8ec1
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-08-28 14:10:55 +0900

    feat: index페이지에 user-list 출력 기능 (#184)
    * feat : index페이지에 user-list 출력 기능
    
    * refactor : 주석 제거

--- src/main/java/com/woowacourse/zzinbros/demo/DemoController.java
+++ src/main/java/com/woowacourse/zzinbros/demo/DemoController.java
@@ -1,6 +1,7 @@
 package com.woowacourse.zzinbros.demo;
 
 import com.woowacourse.zzinbros.post.service.PostService;
+import com.woowacourse.zzinbros.user.service.UserService;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,15 +12,18 @@ import static org.springframework.data.domain.Sort.*;
 @Controller
 public class DemoController {
     private PostService postService;
+    private UserService userService;
 
-    public DemoController(PostService postService) {
+    public DemoController(PostService postService, UserService userService) {
         this.postService = postService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
     public String demo(Model model) {
         Sort sort = by(Direction.DESC, "createdDateTime");
         model.addAttribute("posts", postService.readAll(sort));
+        model.addAttribute("users", userService.readAll());
         return "index";
     }
 
