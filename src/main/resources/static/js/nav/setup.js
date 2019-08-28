8d16b9205eb5ae17fd2d4da4e422eff0b695cab7
pkch93 <star_final@naver.com>
2019-08-28 15:35:24 +0900

    feat: nav 이름 누르면 마이페이지 이동 구현

--- src/main/resources/static/js/nav/setup.js
+++ src/main/resources/static/js/nav/setup.js
@@ -1,6 +1,11 @@
+const MYPAGE_URL = (userId) => `/users/${userId}`
+
 const setupNavBarUserName = () => {
     const currentUserName = document.getElementById("current-user-name")
     currentUserName.innerHTML = localStorage.loginUserName
+    currentUserName.addEventListener('click', (event) => {
+        location.href = MYPAGE_URL(localStorage.loginUserId)
+    })
 }
 
 setupNavBarUserName()
