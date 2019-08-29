bd496befd4c16e51d33c49568b0a35f1d1c3cf49
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-08-29 17:51:04 +0900

    feat : 로그아웃 상태일 때, entrance 페이지로 이동 (#200)

--- src/main/java/com/woowacourse/zzinbros/demo/DemoController.java
+++ src/main/java/com/woowacourse/zzinbros/demo/DemoController.java
@@ -2,6 +2,8 @@ package com.woowacourse.zzinbros.demo;
 
 import com.woowacourse.zzinbros.post.service.PostService;
 import com.woowacourse.zzinbros.user.service.UserService;
+import com.woowacourse.zzinbros.user.web.support.SessionInfo;
+import com.woowacourse.zzinbros.user.web.support.UserSession;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +22,7 @@ public class DemoController {
     }
 
     @GetMapping("/")
-    public String demo(Model model) {
+    public String demo(Model model, @SessionInfo UserSession userSession) {
         Sort sort = by(Direction.DESC, "createdDateTime");
         model.addAttribute("posts", postService.readAll(sort));
         model.addAttribute("users", userService.readAll());
--- src/test/java/com/woowacourse/zzinbros/demo/DemoControllerTest.java
+++ src/test/java/com/woowacourse/zzinbros/demo/DemoControllerTest.java
@@ -1,17 +1,17 @@
 package com.woowacourse.zzinbros.demo;
 
+import com.woowacourse.zzinbros.user.dto.UserResponseDto;
+import com.woowacourse.zzinbros.user.web.support.UserSession;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.nio.charset.StandardCharsets;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -40,10 +40,19 @@ class DemoControllerTest {
     }
 
     @Test
-    void demo() throws Exception {
+    @DisplayName("로그인 안됏을 때 /entrance로 redirect한다")
+    void demoWhenNotLogin() throws Exception {
         mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(new MediaType(MediaType.TEXT_HTML, StandardCharsets.UTF_8)));
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    @DisplayName("로그인 됐을 때 index로 이동")
+    void demoWhenLogin() throws Exception {
+        UserResponseDto loginUserDto = new UserResponseDto(1L, "name", "login@test.com");
+        mockMvc.perform(get("/")
+                .sessionAttr(UserSession.LOGIN_USER, loginUserDto))
+                .andExpect(status().isOk());
     }
 
     @Test
