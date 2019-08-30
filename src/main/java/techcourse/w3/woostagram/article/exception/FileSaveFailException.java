567d7179976f63945fbcd89eb49af72a1c674662
green4469 <green4469@gmail.com>
2019-08-30 10:53:46 +0900

    feat: make exception chained

--- src/main/java/techcourse/w3/woostagram/article/exception/FileSaveFailException.java
+++ src/main/java/techcourse/w3/woostagram/article/exception/FileSaveFailException.java
@@ -3,6 +3,10 @@ package techcourse.w3.woostagram.article.exception;
 public class FileSaveFailException extends RuntimeException {
     private static final String ERROR_FILE_UPLOAD = "파일을 서버에 업로드하는데 실패했습니다.";
 
+    public FileSaveFailException(Throwable cause) {
+        super(ERROR_FILE_UPLOAD, cause);
+    }
+
     public FileSaveFailException() {
         super(ERROR_FILE_UPLOAD);
     }
