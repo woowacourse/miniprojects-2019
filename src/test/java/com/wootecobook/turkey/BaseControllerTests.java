5a7302bca04502135e8cd0af1064a515394ee005
pkch93 <star_final@naver.com>
2019-09-01 15:22:16 +0900

    feat: webTestClient rest docs 설정

--- src/test/java/com/wootecobook/turkey/BaseControllerTests.java
+++ src/test/java/com/wootecobook/turkey/BaseControllerTests.java
@@ -5,13 +5,22 @@ import com.wootecobook.turkey.messenger.service.dto.MessengerRequest;
 import com.wootecobook.turkey.messenger.service.dto.MessengerRoomResponse;
 import com.wootecobook.turkey.user.service.dto.UserRequest;
 import com.wootecobook.turkey.user.service.dto.UserResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 public abstract class BaseControllerTests {
     protected static final String JSESSIONID = "JSESSIONID";
     protected static final String VALID_USER_PASSWORD = "P@ssw0rd";
@@ -23,8 +32,33 @@ public abstract class BaseControllerTests {
     private static final String USER_API_URI = "/api/users";
     private static final String USER_API_URI_WITH_SLASH = USER_API_URI + "/";
 
-    @Autowired
     protected WebTestClient webTestClient;
+
+    protected final ResponseFieldsSnippet pageResponseSnippets = responseFields(
+            fieldWithPath("totalElements").description("총 Post 갯수"),
+            fieldWithPath("numberOfElements").description("현재 페이지에서 조회한 Post 갯수"),
+            fieldWithPath("totalPages").description("총 페이지 갯수"),
+            fieldWithPath("size").description("조회하려는 데이터의 갯수"),
+            fieldWithPath("number").description("현재 페이지 번호"),
+            fieldWithPath("first").description("첫번째 페이지 여부"),
+            fieldWithPath("last").description("마지막 페이지 여부"),
+            fieldWithPath("empty").description("현재 페이지 Post 정보 유무 여부")
+    );
+
+    protected final ResponseFieldsSnippet badRequestSnippets = responseFields(
+            fieldWithPath("message").description("에러 메세지")
+    );
+
+
+    @BeforeEach
+    void setUp(final RestDocumentationContextProvider restDocumentation) {
+        this.webTestClient = WebTestClient.bindToServer()
+                .filter(documentationConfiguration(restDocumentation)
+                        .operationPreprocessors()
+                        .withRequestDefaults(prettyPrint())
+                        .withResponseDefaults(prettyPrint()))
+                .build();
+    }
 
     protected Long addUser(String name, String email, String password) {
         UserRequest userRequest = UserRequest.builder()
