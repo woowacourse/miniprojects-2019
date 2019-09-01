085ecbed7e9e223654446e3bfb3e85c02b9335d6
pkch93 <star_final@naver.com>
2019-09-01 15:23:57 +0900

    feat: PostApiController 문서화

--- src/test/java/com/wootecobook/turkey/post/controller/PostApiControllerTests.java
+++ src/test/java/com/wootecobook/turkey/post/controller/PostApiControllerTests.java
@@ -10,31 +10,44 @@ import com.wootecobook.turkey.post.service.dto.PostResponse;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 import reactor.core.publisher.Mono;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
 
 @Import(AwsMockConfig.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class PostApiControllerTests extends BaseControllerTests {
 
     public static final String POST_URL = "/api/posts";
 
-    @Autowired
-    private WebTestClient webTestClient;
-
     private Long authorId;
     private String authorJSessionId;
+
     private Long otherId;
     private String otherJSessionId;
+
     private ByteArrayResource testFile;
+
+    private final ResponseFieldsSnippet postResponseSnippets = responseFields(
+            fieldWithPath("id").description("글의 고유 id"),
+            fieldWithPath("contents.contents").description("글의 내용"),
+            fieldWithPath("createdAt").description("글 작성 일자"),
+            fieldWithPath("updatedAt").description("글 수정 일자"),
+            fieldWithPath("totalComment").description("글에 달린 댓글의 총 갯수"),
+            subsectionWithPath("files").description("글과 함께 업로드 된 사진 또는 동영상 정보"),
+            subsectionWithPath("author").description("작성자 정보"),
+            subsectionWithPath("receiver").optional().description("글 받는 사람 정보"),
+            subsectionWithPath("goodResponse").description("해당 글의 좋아요 정보")
+    );
 
     @BeforeEach
     void setUp() {
@@ -65,6 +78,14 @@ class PostApiControllerTests extends BaseControllerTests {
                 .exchange()
                 .expectStatus().isCreated()
                 .expectBody(PostResponse.class)
+                .consumeWith(document("post/201/create/withfiles",
+                        requestParts(
+                                partWithName("contents").description("글의 내용"),
+                                partWithName("files").optional().description("글과 함께 업로드하는 사진 또는 동영상, 여러장 가능"),
+                                partWithName("receiver").optional().description("다른 유저에게 글쓰는 경우, 해당 유저의 id")
+                        ),
+                        postResponseSnippets
+                ))
                 .returnResult()
                 .getResponseBody();
 
@@ -86,6 +107,14 @@ class PostApiControllerTests extends BaseControllerTests {
                 .exchange()
                 .expectStatus().isCreated()
                 .expectBody(PostResponse.class)
+                .consumeWith(document("post/201/create/nofiles",
+                        requestParts(
+                                partWithName("contents").description("글의 내용"),
+                                partWithName("files").optional().description("글과 함께 업로드하는 사진 또는 동영상, 여러장 가능"),
+                                partWithName("receiver").optional().description("다른 유저에게 글쓰는 경우, 해당 유저의 id")
+                        ),
+                        postResponseSnippets
+                ))
                 .returnResult()
                 .getResponseBody();
 
@@ -107,6 +136,9 @@ class PostApiControllerTests extends BaseControllerTests {
                 .exchange()
                 .expectStatus().isBadRequest()
                 .expectBody(ErrorMessage.class)
+                .consumeWith(document("post/400/no-content/create",
+                        badRequestSnippets
+                ))
                 .returnResult()
                 .getResponseBody();
 
@@ -117,10 +149,22 @@ class PostApiControllerTests extends BaseControllerTests {
     @Test
     void 페이지_조회_정상_로직_테스트() {
         //when & then
-        webTestClient.get().uri(POST_URL)
+        webTestClient.get().uri(POST_URL + "?page=0")
                 .cookie(JSESSIONID, authorJSessionId)
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .consumeWith(document("post/200/read",
+                        relaxedRequestParameters(
+                                parameterWithName("page").description("페이지 번호")
+                        ),
+                        pageResponseSnippets.and(
+                                subsectionWithPath("pageable").description("페이지 정보"),
+                                subsectionWithPath("content").description("조회하고자 하는 해당 페이지의 Post 목록"),
+                                subsectionWithPath("sort").description("조회 정렬 정보")
+                        )
+                ));
     }
 
     @Test
@@ -138,6 +182,15 @@ class PostApiControllerTests extends BaseControllerTests {
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(PostResponse.class)
+                .consumeWith(document("post/200/update",
+                        pathParameters(
+                                parameterWithName("postId").description("수정할 Post의 id")
+                        ),
+                        relaxedRequestFields(
+                                fieldWithPath("contents").description("수정할 글의 내용")
+                        ),
+                        postResponseSnippets
+                ))
                 .returnResult()
                 .getResponseBody();
 
@@ -174,7 +227,11 @@ class PostApiControllerTests extends BaseControllerTests {
                 .contentType(MEDIA_TYPE)
                 .body(Mono.just(postUpdateRequest), PostRequest.class)
                 .exchange()
-                .expectStatus().isBadRequest();
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .consumeWith(document("post/400/no-auth/update",
+                        badRequestSnippets
+                ));;
     }
 
     @Test
@@ -186,7 +243,13 @@ class PostApiControllerTests extends BaseControllerTests {
         webTestClient.delete().uri(POST_URL + "/{postId}", postId)
                 .cookie(JSESSIONID, authorJSessionId)
                 .exchange()
-                .expectStatus().isNoContent();
+                .expectStatus().isNoContent()
+                .expectBody()
+                .consumeWith(document("post/204/delete",
+                        pathParameters(
+                                parameterWithName("postId").description("해당 Post의 고유 식별 id")
+                        )
+                ));
     }
 
     @Test
@@ -195,7 +258,14 @@ class PostApiControllerTests extends BaseControllerTests {
         webTestClient.delete().uri(POST_URL + "/{postId}", Long.MAX_VALUE)
                 .cookie(JSESSIONID, authorJSessionId)
                 .exchange()
-                .expectStatus().isBadRequest();
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .consumeWith(document("post/400/delete/none",
+                        pathParameters(
+                                parameterWithName("postId").description("해당 Post의 고유 식별 id")
+                        ),
+                        badRequestSnippets
+                ));
     }
 
     @Test
@@ -207,7 +277,14 @@ class PostApiControllerTests extends BaseControllerTests {
         webTestClient.delete().uri(POST_URL + "/{postId}", postId)
                 .cookie(JSESSIONID, otherJSessionId)
                 .exchange()
-                .expectStatus().isBadRequest();
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .consumeWith(document("post/400/delete/no-auth",
+                        pathParameters(
+                                parameterWithName("postId").description("해당 Post의 고유 식별 id")
+                        ),
+                        badRequestSnippets
+                ));
     }
 
     @Test
@@ -221,6 +298,15 @@ class PostApiControllerTests extends BaseControllerTests {
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(GoodResponse.class)
+                .consumeWith(document("post/200/good/create",
+                        pathParameters(
+                                parameterWithName("postId").description("해당 Post의 고유 식별 id")
+                        ),
+                        responseFields(
+                                fieldWithPath("totalGood").description("해당 게시글의 총 좋아요 갯수"),
+                                fieldWithPath("gooded").description("현재 로그인 한 유저의 해당 Post 좋아요 여부")
+                        )
+                ))
                 .returnResult()
                 .getResponseBody();
 
@@ -239,7 +325,6 @@ class PostApiControllerTests extends BaseControllerTests {
         // then
         assertThat(postGoodCancelResponse.getTotalGood()).isEqualTo(0);
     }
-
 
     private Long addPost(String contents) {
         //given
