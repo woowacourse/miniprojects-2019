746f01743e91d883a9d9fe8d06165b67973139c9
school0bhy <38012258+school0bhy@users.noreply.github.com>
2019-09-03 08:55:55 +0900

    refactor: 채팅방 페이지 요청 에러 발생 처리 (#128)
    - 403 에러 페이지로 리다이렉트

--- src/main/java/com/wootecobook/turkey/messenger/service/exception/AccessDeniedException.java
+++ src/main/java/com/wootecobook/turkey/messenger/service/exception/AccessDeniedException.java
@@ -1,10 +1,4 @@
 package com.wootecobook.turkey.messenger.service.exception;
 
 public class AccessDeniedException extends RuntimeException {
-
-    private static final String ACCESS_DENIED_MESSAGE = "접근 권한이 없습니다";
-
-    public AccessDeniedException() {
-        super(ACCESS_DENIED_MESSAGE);
-    }
 }
