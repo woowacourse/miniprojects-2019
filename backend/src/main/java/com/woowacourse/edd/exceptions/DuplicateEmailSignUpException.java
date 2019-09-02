1f6bab009a3bb25838ee0230370c47380ba17762
kimjungmin-developer <50177674+kimjungmin-developer@users.noreply.github.com>
2019-09-02 14:05:03 +0900

    [fix] fix duplicatEmailSignUpException (#233)
    - 이메일 중복 메세지 내용을 변경했습니다.

--- backend/src/main/java/com/woowacourse/edd/exceptions/DuplicateEmailSignUpException.java
+++ backend/src/main/java/com/woowacourse/edd/exceptions/DuplicateEmailSignUpException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class DuplicateEmailSignUpException extends ErrorResponseException {
 
-    public static final String DUPLICATE_EMAIL_SIGNUP_MESSAGE = "이미 존재하는 이메일입니다.";
+    public static final String DUPLICATE_EMAIL_SIGNUP_MESSAGE = "이미 존재하는 이메일입니다(탈퇴한 이메일도 사용할 수 없습니다)";
 
     public DuplicateEmailSignUpException() {
         super(DUPLICATE_EMAIL_SIGNUP_MESSAGE, HttpStatus.BAD_REQUEST);
