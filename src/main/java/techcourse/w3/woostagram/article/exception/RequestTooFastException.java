c693fb256081e9f68632e5cc2c0d64abbc0f5f9f
김성훈 <seonghun127@gmail.com>
2019-09-02 19:23:29 +0900

    refactor: resolve conflict

--- src/main/java/techcourse/w3/woostagram/article/exception/RequestTooFastException.java
+++ src/main/java/techcourse/w3/woostagram/article/exception/RequestTooFastException.java
@@ -1,8 +1,8 @@
 package techcourse.w3.woostagram.article.exception;
 
-import techcourse.w3.woostagram.common.exception.WoostagramExeception;
+import techcourse.w3.woostagram.common.exception.WoostagramException;
 
-public class RequestTooFastException extends WoostagramExeception {
+public class RequestTooFastException extends WoostagramException {
     private static final String ERROR_TOO_FAST = "요청이 너무 잦습니다. 잠시 후 시도해주세요.";
 
     public RequestTooFastException() {
