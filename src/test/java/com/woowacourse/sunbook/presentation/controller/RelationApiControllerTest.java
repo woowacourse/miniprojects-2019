991dfc41c1b8721df1fac88e49977869528d7960
hyunssooo <jhs695@naver.com>
2019-09-02 19:17:33 +0900

    [테스트] 테스트 코드 추가 (#55)
    * refactor: 메소드 분리, URL 변경
    
    * refactor: 반환값 수정
    
    * refactor: url 변경
    
    * feat: id로 유저 찾는 메소드 추가
    
    * feat: 테스트코드 수정
    
    * feat: 유저 페이지에서 친구 추가 프론트 추가
    
    * refactor: signout 어노테이션 변경 post -> get
    
    * refactor: html 중복 제거
    
    * refactor: 유저 프로필 이동 경로 변경
    
    * refactor: 설정 수정
    
    * refactor: 피드백 반영 사용하지 않는 함수 제거
    
    * refactor: 피드백 반영
    
    * feat: friend List 프론트 추가
    
    * refactor: search 페이지 친구 리스트 추가
    
    * fix: 로그인 이메일이 너무 긴 경우 응답 늦음 버그 수정
    
    * feat: 테스트 코드 추가
    
    * feat: 테스트 코드 추가

--- src/test/java/com/woowacourse/sunbook/presentation/controller/RelationApiControllerTest.java
+++ src/test/java/com/woowacourse/sunbook/presentation/controller/RelationApiControllerTest.java
@@ -5,6 +5,7 @@ import com.woowacourse.sunbook.presentation.template.TestTemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 class RelationApiControllerTest extends TestTemplate {
 
@@ -13,5 +14,85 @@ class RelationApiControllerTest extends TestTemplate {
         String sessionId = loginSessionId(userRequestDto);
         respondApi(loginAndRequest(HttpMethod.GET, "/api/friends/2", Relationship.NONE, HttpStatus.OK, sessionId))
                 .jsonPath("$..relationship").isEqualTo("NONE");
+    }
+
+    @Test
+    void 친구_수락() {
+        String sessionId = loginSessionId(userRequestDto);
+        respondApi(loginAndRequest(HttpMethod.POST, "/api/friends/2", Relationship.NONE, HttpStatus.OK, sessionId))
+                .consumeWith(signOut -> {
+                    webTestClient.method(HttpMethod.GET).uri("/signout")
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .exchange()
+                            .expectBody()
+                            .consumeWith(userLogin -> {
+                                String userSessionId = loginSessionId(otherRequestDto);
+                                respondApi(loginAndRequest(HttpMethod.PUT, "/api/friends/1", Relationship.NONE, HttpStatus.OK, userSessionId))
+                                        .jsonPath("$..relationship").isEqualTo("FRIEND");
+                            });
+                })
+                .consumeWith(deleteRelation -> {
+                    String userSessionId = loginSessionId(otherRequestDto);
+                    respondApi(loginAndRequest(HttpMethod.DELETE, "api/friends/1", Relationship.NONE, HttpStatus.OK, userSessionId));
+                });
+    }
+
+    @Test
+    void 친구_거절() {
+        String sessionId = loginSessionId(userRequestDto);
+        respondApi(loginAndRequest(HttpMethod.POST, "/api/friends/2", Relationship.NONE, HttpStatus.OK, sessionId))
+                .consumeWith(signOut -> {
+                    webTestClient.method(HttpMethod.GET).uri("/signout")
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .exchange()
+                            .expectBody()
+                            .consumeWith(userLogin -> {
+                                String userSessionId = loginSessionId(otherRequestDto);
+                                respondApi(loginAndRequest(HttpMethod.DELETE, "/api/friends/1", Relationship.NONE, HttpStatus.OK, userSessionId))
+                                        .jsonPath("$..relationship").isEqualTo("NONE");
+                            });
+                });
+    }
+
+    @Test
+    void 친구_요청_받은_리스트() {
+        String sessionId = loginSessionId(userRequestDto);
+        respondApi(loginAndRequest(HttpMethod.POST, "/api/friends/2", Relationship.NONE, HttpStatus.OK, sessionId))
+                .consumeWith(signOut -> {
+                    webTestClient.method(HttpMethod.GET).uri("/signout")
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .exchange()
+                            .expectBody()
+                            .consumeWith(userLogin -> {
+                                String userSessionId = loginSessionId(otherRequestDto);
+                                respondApi(loginAndRequest(HttpMethod.GET, "/api/friends/friends/requested", Void.class, HttpStatus.OK, userSessionId))
+                                        .jsonPath("$.length()").isEqualTo("1");
+                            });
+                })
+                .consumeWith(deleteRelation -> {
+                    String userSessionId = loginSessionId(otherRequestDto);
+                    respondApi(loginAndRequest(HttpMethod.DELETE, "api/friends/1", Relationship.NONE, HttpStatus.OK, userSessionId));
+                });
+    }
+
+    @Test
+    void 친구_리스트() {
+        String sessionId = loginSessionId(userRequestDto);
+        respondApi(loginAndRequest(HttpMethod.POST, "/api/friends/2", Relationship.NONE, HttpStatus.OK, sessionId))
+                .consumeWith(signOut -> {
+                    webTestClient.method(HttpMethod.GET).uri("/signout")
+                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+                            .exchange()
+                            .expectBody()
+                            .consumeWith(userLogin -> {
+                                String userSessionId = loginSessionId(otherRequestDto);
+                                respondApi(loginAndRequest(HttpMethod.GET, "/api/friends/friends", Void.class, HttpStatus.OK, userSessionId))
+                                        .jsonPath("$.length()").isEqualTo("0");
+                            });
+                })
+                .consumeWith(deleteRelation -> {
+                    String userSessionId = loginSessionId(otherRequestDto);
+                    respondApi(loginAndRequest(HttpMethod.DELETE, "api/friends/1", Relationship.NONE, HttpStatus.OK, userSessionId));
+                });
     }
 }
