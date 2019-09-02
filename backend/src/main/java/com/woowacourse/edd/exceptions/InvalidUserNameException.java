19686b88bf774e0329e922fa564157702b511faa
Sa Myungki <conatuseus@gmail.com>
2019-09-02 16:16:20 +0900

    [fix] fix message for name (#231)
    - 닉네임이 영어, 한글이 가능하다고 변경

--- backend/src/main/java/com/woowacourse/edd/exceptions/InvalidUserNameException.java
+++ backend/src/main/java/com/woowacourse/edd/exceptions/InvalidUserNameException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class InvalidUserNameException extends ErrorResponseException {
 
-    public static final String INVALID_USER_NAME_MESSAGE = "올바르지 않은 사용자 이름입니다.";
+    public static final String INVALID_USER_NAME_MESSAGE = "이름은 영어 또는 한글만 가능합니다.";
 
     public InvalidUserNameException() {
         super(INVALID_USER_NAME_MESSAGE, HttpStatus.BAD_REQUEST);
