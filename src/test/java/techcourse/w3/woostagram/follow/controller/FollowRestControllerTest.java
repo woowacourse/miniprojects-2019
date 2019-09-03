7bacdfd179ecc85cc0c7b01f920e53ec70b279a3
김성훈 <seonghun127@gmail.com>
2019-09-03 15:32:44 +0900

    refactor: refact test code convetion

--- src/test/java/techcourse/w3/woostagram/follow/controller/FollowRestControllerTest.java
+++ src/test/java/techcourse/w3/woostagram/follow/controller/FollowRestControllerTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static techcourse.w3.woostagram.common.support.TestDataInitializer.*;
 
 class FollowRestControllerTest extends AbstractControllerTests {
+
     @Override
     @BeforeEach
     protected void setUp() {
